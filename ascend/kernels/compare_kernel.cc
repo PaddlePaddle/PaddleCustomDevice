699611b6af91cb7051effeea5eeb7aa39a707661
Aganlengzi <aganlengzi@gmail.com>
2022-03-31 05:25:15 +0800

    add kernels

---
+++ ascend/kernels/compare_kernel.cc
@@ -0,0 +1,134 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "npu_op_runner.h"
+
+namespace custom_kernel {
+
+template <typename T, typename Context>
+void EqualKernel(const Context& dev_ctx,
+                 const phi::DenseTensor& x,
+                 const phi::DenseTensor& y,
+                 int axis,
+                 phi::DenseTensor* out) {
+  dev_ctx.template Alloc<bool>(out);
+  const auto& runner = NpuOpRunner("Equal", {x, y}, {*out}, {});
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void NotEqualKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& x,
+                    const phi::DenseTensor& y,
+                    int axis,
+                    phi::DenseTensor* out) {
+  dev_ctx.template Alloc<bool>(out);
+  const auto& runner = NpuOpRunner("Equal", {x, y}, {*out}, {});
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void LessThanKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& x,
+                    const phi::DenseTensor& y,
+                    int axis,
+                    phi::DenseTensor* out) {
+  dev_ctx.template Alloc<bool>(out);
+  const auto& runner = NpuOpRunner("Less", {x, y}, {*out}, {});
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void GreaterEqualKernel(const Context& dev_ctx,
+                        const phi::DenseTensor& x,
+                        const phi::DenseTensor& y,
+                        int axis,
+                        phi::DenseTensor* out) {
+  dev_ctx.template Alloc<bool>(out);
+  const auto& runner = NpuOpRunner("GreaterEqual", {x, y}, {*out}, {});
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void GreaterThanKernel(const Context& dev_ctx,
+                       const phi::DenseTensor& x,
+                       const phi::DenseTensor& y,
+                       int axis,
+                       phi::DenseTensor* out) {
+  dev_ctx.template Alloc<bool>(out);
+  const auto& runner = NpuOpRunner("Greater", {x, y}, {*out}, {});
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+}  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(equal,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::EqualKernel,
+                          bool,
+                          int16_t,
+                          int,
+                          int64_t,
+                          float,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(not_equal,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::NotEqualKernel,
+                          bool,
+                          int16_t,
+                          int,
+                          int64_t,
+                          float,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(less_than,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::LessThanKernel,
+                          bool,
+                          int16_t,
+                          int,
+                          int64_t,
+                          float,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(greater_equal,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::GreaterEqualKernel,
+                          bool,
+                          int16_t,
+                          int,
+                          int64_t,
+                          float,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(greater_than,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::GreaterThanKernel,
+                          bool,
+                          int16_t,
+                          int,
+                          int64_t,
+                          float,
+                          double) {}
---
+++ ascend/kernels/top_k_kernel.cc
@@ -0,0 +1,80 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "npu_funcs.h"
+#include "npu_op_runner.h"
+
+namespace custom_kernel {
+
+template <typename T, typename Context>
+void TopkKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                const phi::Scalar& k_scalar,
+                int axis,
+                bool largest,
+                bool sorted,
+                phi::DenseTensor* out,
+                phi::DenseTensor* indices) {
+  if (axis < 0) {
+    axis += x.dims().size();
+  }
+
+  int k = k_scalar.to<int>();
+
+  phi::DDim output_dims = x.dims();
+  output_dims[axis] = k;
+
+  out->Resize(output_dims);
+  indices->Resize(output_dims);
+
+  dev_ctx.template Alloc<T>(out);
+  dev_ctx.template Alloc<int64_t>(indices);
+
+  phi::DenseTensor indices_int32(paddle::experimental::DataType::INT32);
+  indices_int32.Resize(output_dims);
+  dev_ctx.template Alloc<int32_t>(&indices_int32);
+
+  auto npu_stream = dev_ctx.stream();
+
+  NpuOpRunner npu_op_runner_topkv2;
+  npu_op_runner_topkv2.SetType("TopKV2")
+      .AddInput(x)
+      .AddInput(std::vector<int32_t>{k})
+      .AddOutput(*out)
+      .AddOutput(indices_int32)
+      .AddAttr("sorted", sorted)
+      .AddAttr("dim", axis)
+      .AddAttr("largest", largest)
+      .Run(npu_stream);
+
+  // Cast 'indices_int32' to 'indices', from INT32 to INT64
+  auto dst_dtype = ConvertToNpuDtype(indices->type());
+  const auto& npu_op_runner_cast =
+      NpuOpRunner("Cast",
+                  {indices_int32},
+                  {*indices},
+                  {{"dst_type", static_cast<int>(dst_dtype)}});
+  npu_op_runner_cast.Run(npu_stream);
+}
+
+}  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(top_k,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::TopkKernel,
+                          float,
+                          double,
+                          int,
+                          int64_t) {}

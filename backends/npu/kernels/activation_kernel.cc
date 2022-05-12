82d63c75e729d7f25aeaeedd690e5a26ff23be08
furnace <34057289+windstamp@users.noreply.github.com>
2022-05-12 14:59:39 +0800

    Add ops for yolov3 (#15)
    * Add ops for yolov3
    
    * fix 2021 to 2022

--- backends/npu/kernels/activation_kernel.cc
+++ backends/npu/kernels/activation_kernel.cc
@@ -18,6 +18,29 @@
 namespace custom_kernel {
 
 template <typename T, typename Context>
+void ExpKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                phi::DenseTensor* out) {
+  dev_ctx.template Alloc<T>(out);
+  auto stream = dev_ctx.stream();
+
+  const auto& runner = NpuOpRunner("Exp", {x}, {*out}, {});
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void ExpGradKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& out,
+                    const phi::DenseTensor& dout,
+                    phi::DenseTensor* dx) {
+  dev_ctx.template Alloc<T>(dx);
+  auto stream = dev_ctx.stream();
+
+  const auto& runner = NpuOpRunner("Mul", {dout, out}, {*dx}, {});
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
 void ReluKernel(const Context& dev_ctx,
                 const phi::DenseTensor& x,
                 phi::DenseTensor* out) {
@@ -39,7 +62,67 @@ void ReluGradKernel(const Context& dev_ctx,
   runner.Run(stream);
 }
 
+template <typename T, typename Context>
+void LeakyReluKernel(const Context& dev_ctx,
+                     const phi::DenseTensor& x,
+                     float alpha,
+                     phi::DenseTensor* out) {
+  dev_ctx.template Alloc<T>(out);
+
+  auto stream = dev_ctx.stream();
+  const auto& runner =
+      NpuOpRunner("LeakyRelu", {x}, {*out}, {{"negative_slope", alpha}});
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void LeakyReluGradKernel(const Context& dev_ctx,
+                         const phi::DenseTensor& x,
+                         const phi::DenseTensor& dout,
+                         float alpha,
+                         phi::DenseTensor* dx) {
+  dev_ctx.template Alloc<T>(dx);
+
+  auto stream = dev_ctx.stream();
+  const auto& runner = NpuOpRunner("LeakyReluGrad", {dout, x}, {*dx},
+                                     {{"negative_slope", alpha}});
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void SigmoidKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                phi::DenseTensor* out) {
+  dev_ctx.template Alloc<T>(out);
+  auto stream = dev_ctx.stream();
+
+  const auto& runner = NpuOpRunner("Sigmoid", {x}, {*out}, {});
+  runner.Run(stream);
+}
+
+template <typename T, typename Context>
+void SigmoidGradKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& out,
+                    const phi::DenseTensor& dout,
+                    phi::DenseTensor* dx) {
+  dev_ctx.template Alloc<T>(dx);
+  auto stream = dev_ctx.stream();
+
+  const auto& runner = NpuOpRunner("SigmoidGrad", {out, dout}, {*dx}, {});
+  runner.Run(stream);
+}
+
 }  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(
+    exp, ascend, ALL_LAYOUT, custom_kernel::ExpKernel, float, double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(exp_grad,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::ExpGradKernel,
+                          float,
+                          double) {}
 
 PD_REGISTER_PLUGIN_KERNEL(
     relu, ascend, ALL_LAYOUT, custom_kernel::ReluKernel, float, double) {}
@@ -49,4 +132,32 @@ PD_REGISTER_PLUGIN_KERNEL(relu_grad,
                           ALL_LAYOUT,
                           custom_kernel::ReluGradKernel,
                           float,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(
+    leaky_relu, ascend, ALL_LAYOUT, custom_kernel::LeakyReluKernel,
+    float,
+    phi::dtype::float16,
+    double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(leaky_relu_grad,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::LeakyReluGradKernel,
+                          float,
+                          phi::dtype::float16,
+                          double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(
+    sigmoid, ascend, ALL_LAYOUT, custom_kernel::SigmoidKernel,
+    float,
+    phi::dtype::float16,
+    double) {}
+
+PD_REGISTER_PLUGIN_KERNEL(sigmoid_grad,
+                          ascend,
+                          ALL_LAYOUT,
+                          custom_kernel::SigmoidGradKernel,
+                          float,
+                          phi::dtype::float16,
                           double) {}

f3cb202a19ec37defeddee6076959d000fc6e79f
Aganlengzi <aganlengzi@gmail.com>
2022-07-08 02:33:07 +0800

    npu runtime add AddCallbac

--- backends/npu/runtime/runtime.cc
+++ backends/npu/runtime/runtime.cc
@@ -318,6 +318,14 @@ C_Status StreamWaitEvent(const C_Device device,
   return C_SUCCESS;
 }
 
+C_Status AddCallback(const C_Device device,
+                     C_Stream stream,
+                     C_Callback callback,
+                     void* user_data) {
+  ACL_CHECK(aclrtSynchronizeStream(reinterpret_cast<aclrtStream>(stream)));
+  return C_SUCCESS;
+}
+
 C_Status DeviceMemStats(const C_Device device,
                         size_t *total_memory,
                         size_t *free_memory) {
@@ -370,6 +378,7 @@ void InitPlugin(CustomRuntimeParams *params) {
   params->interface->synchronize_stream = SyncStream;
   params->interface->synchronize_event = SyncEvent;
   params->interface->stream_wait_event = StreamWaitEvent;
+  params->interface->stream_add_callback = AddCallback;
 
   params->interface->memory_copy_h2d = MemCpyH2D;
   params->interface->memory_copy_d2d = MemCpyD2D;

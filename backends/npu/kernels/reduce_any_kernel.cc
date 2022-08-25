5c28dba2d9bcb91a5b4a2cf9d8a1face5da8485f
USTCKAY <1136780339@qq.com>
2022-08-25 20:30:59 +0800

    add 3 ops

---
+++ backends/npu/kernels/reduce_any_kernel.cc
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "kernels/funcs/npu_funcs.h"
+#include "kernels/funcs/npu_op_runner.h"
+
+namespace custom_kernel {
+
+template <typename T, typename Context>
+void AnyKernel(const Context& dev_ctx,
+               const phi::DenseTensor& x,
+               const std::vector<int64_t>& dims,
+               bool keep_dim,
+               phi::DenseTensor* out) {
+  dev_ctx.template Alloc<T>(out);
+
+  // set attr
+  NPUAttributeMap attr = {{"keep_dims", keep_dim}, {"axes", dims}};
+
+  const auto& runner = NpuOpRunner("ReduceAnyD", {x}, {*out}, attr);
+  auto stream = dev_ctx.stream();
+  runner.Run(stream);
+}
+
+}  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(
+    any, ascend, ALL_LAYOUT, custom_kernel::AnyKernel, bool) {}

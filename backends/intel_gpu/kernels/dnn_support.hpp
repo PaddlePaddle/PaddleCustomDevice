5b548deeb0f5bddab4e8d7e2b9017ef91d54e170
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2022-08-26 21:20:19 +0800

    update logs

--- backends/intel_gpu/kernels/dnn_support.hpp
+++ backends/intel_gpu/kernels/dnn_support.hpp
@@ -1,5 +1,16 @@
 #pragma once
 #include <CL/sycl.hpp>
+#include <thread>
+#include <vector>
+
+#include "oneapi/dnnl/dnnl_sycl.hpp"
+#define show_msg(title, x)                                                    \
+  std::cout << "[" << title << "][" << std::hex << std::this_thread::get_id() \
+            << std::dec << "]["<< __FILE__<< ":"<< __LINE__ <<"]: " << x << std::endl;
+
+// #define show(x) show_msg("SHOW", x)
+#define show(x)
+#define show_kernel(x) show_msg("KERNEL", x)
 
 namespace dnn_support {
 
@@ -21,5 +32,13 @@ struct toDnnType<char> {
   const static dnnl::memory::data_type type = dnnl::memory::data_type::bf16;
 };
 
+/*
+template <>
+struct toDnnType<double> {
+  const static dnnl::memory::data_type type = dnnl::memory::data_type::f64;
+};
+
+*/
 
 }
+

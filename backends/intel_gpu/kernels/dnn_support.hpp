f0aa2b767d8c3d487b553f1d9d883fa19227d3b2
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2022-09-02 00:42:58 +0800

    add PLUGIN_CHUNK_SIZE, PLUGIN_VERBOSE options

--- backends/intel_gpu/kernels/dnn_support.hpp
+++ backends/intel_gpu/kernels/dnn_support.hpp
@@ -4,15 +4,66 @@
 #include <vector>
 
 #include "oneapi/dnnl/dnnl_sycl.hpp"
-#define show_msg(title, x)                                                    \
+
+namespace config {
+template<int v>
+struct DeviceConfig {
+  size_t chunk_size;
+  size_t plugin_verbose;
+
+  template <class T>
+  T getEnvValue(const char* name, T defaultValue) {
+    T ret = defaultValue;
+
+    auto p = std::getenv(name);
+
+    if (p) {
+      std::stringstream ss;
+      ss << p;
+      ss >> ret;
+      if (ss.fail()) {
+        throw std::runtime_error("Can't convert type");
+      }
+    }
+
+    return ret;
+  }
+
+  DeviceConfig() : chunk_size{4}, plugin_verbose{0} {
+    chunk_size = getEnvValue("PLUGIN_CHUNK_SIZE", chunk_size);
+    plugin_verbose = getEnvValue("PLUGIN_VERBOSE", plugin_verbose);
+  }
+};
+
+}
+
+
+using DeviceConfig = config::DeviceConfig<0>;
+using DeviceConfigPtr = std::unique_ptr<DeviceConfig>;
+extern DeviceConfigPtr devconf;
+extern std::mutex mx;
+
+inline void InitializeDevConf() {
+     if(!devconf)
+     {
+        std::lock_guard<decltype(mx)> l(mx);
+        if(!devconf)
+        {
+          devconf = std::make_unique<DeviceConfig>();
+        }
+     }
+}
+
+#define show_msg(title, vbit, x) \
+  if(devconf && devconf->plugin_verbose & vbit) {  \
   std::cout << "[" << title << "][" << std::hex << std::this_thread::get_id() \
-            << std::dec << "]["<< __FILE__<< ":"<< __LINE__ <<"]: " << x << std::endl;
+            << std::dec << "]["<< __FILE__<< ":"<< __LINE__ <<"]: " << x << std::endl; }
 
-// #define show(x) show_msg("SHOW", x)
-#define show(x)
-#define show_kernel(x) show_msg("KERNEL", x)
+#define show(x) show_msg("SHOW", 1, x )
+//#define show(x)
+#define show_kernel(x) show_msg("KERNEL", 2, x)
 
-namespace dnn_support {
+    namespace dnn_support {
 
 template <class T>
 struct toDnnType {};
@@ -39,6 +90,4 @@ struct toDnnType<double> {
 };
 
 */
-
 }
-

2ea67951432a1987a8bb23a47478443fbf5fd0ae
duanyanhui <duanyanhui@baidu.com>
2022-10-18 14:19:43 +0800

    fix bug

--- backends/npu/kernels/funcs/npu_funcs.h
+++ backends/npu/kernels/funcs/npu_funcs.h
@@ -173,12 +173,7 @@ inline void TensorFromVector(const phi::CustomContext& ctx,
             << ", size: " << size;
     std::memcpy(dst_ptr, src_ptr, size);
   } else if (dst_place.GetType() == phi::AllocationType::CUSTOM) {
-    AsyncMemCpyH2D(nullptr,
-                   static_cast<C_Stream>(dev_ctx.stream()),
-                   dst_ptr,
-                   src_ptr,
-                   size);
-    dev_ctx.Wait();
+    MemCpyH2D(nullptr, dst_ptr, src_ptr, size);
   } else {
     PADDLE_THROW(phi::errors::Unimplemented(
         "TensorFromVector on %s is not supported.", dst_place));

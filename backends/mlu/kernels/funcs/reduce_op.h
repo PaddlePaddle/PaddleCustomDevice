98c9fe262b5d0861b492e070b2be60dc35bdafba
ShawnNew <ncxinhanzhong@gmail.com>
2022-10-31 11:46:23 +0000

    [MLU] fix reduce axis null case.

--- backends/mlu/kernels/funcs/reduce_op.h
+++ backends/mlu/kernels/funcs/reduce_op.h
@@ -36,6 +36,8 @@ void MLUReduceOp(const Context& dev_ctx,
     for (size_t i = 0; i < input_dims.size(); i++) {
       reduce_dims.push_back(static_cast<int>(i));
     }
+  } else if (dims.size() == 0) {
+    reduce_dims.push_back(0);
   } else {
     for (size_t i = 0; i < dims.size(); ++i) {
       if (dims[i] < 0) {

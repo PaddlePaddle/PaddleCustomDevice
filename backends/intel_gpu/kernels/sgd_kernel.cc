4fa4fe0501adac944250685a8d42bde6823dce79
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2022-11-04 00:01:26 +0800

    sgd kernel

--- backends/intel_gpu/kernels/sgd_kernel.cc
+++ backends/intel_gpu/kernels/sgd_kernel.cc
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#include "dnn_support.hpp"
 #include "paddle/phi/capi/all.h"
 
 namespace custom_kernel {
 
 template <typename T>
-void sgd_dense_param_dense_grad_impl(const phi::DenseTensor& param,
+void sgd_dense_param_dense_grad_impl(sycl::queue* q, const phi::DenseTensor& param,
                                      const phi::DenseTensor& learning_rate,
                                      const phi::DenseTensor& grad,
                                      phi::DenseTensor* param_out) {
@@ -27,9 +27,17 @@ void sgd_dense_param_dense_grad_impl(const phi::DenseTensor& param,
   const T* grad_data = grad.data<T>();
   T* out_data = param_out->data<T>();
 
-  for (auto i = 0; i < sz; ++i) {
-    out_data[i] = param_data[i] - *lr * grad_data[i];
-  }
+  // for (auto i = 0; i < sz; ++i) {
+  //   out_data[i] = param_data[i] - *lr * grad_data[i];
+  // }
+
+   q->parallel_for(sz, [=](auto& i){
+
+          out_data[i] = param_data[i] - *lr * grad_data[i];
+   });
+
+  q->wait();
+
 }
 
 template <typename T>
@@ -41,11 +49,18 @@ void SGDDenseKernel(const phi::Context& dev_ctx,
                     bool multi_precision,
                     phi::DenseTensor* param_out,
                     phi::DenseTensor* master_param_out) {
+  show_kernel("sgd " << dnn_support::type2String<T>::name() );
+
+  auto* q = static_cast<sycl::queue*>(const_cast<void*>(dev_ctx.stream()));
+
+  if (!q) {
+  }
+
   dev_ctx.template Alloc<T>(param_out);
-  sgd_dense_param_dense_grad_impl<T>(param, learning_rate, grad, param_out);
+  sgd_dense_param_dense_grad_impl<T>(q,param, learning_rate, grad, param_out);
 }
 }  // namespace custom_kernel
 
 PD_BUILD_PHI_KERNEL(
-    sgd, custom_cpu, ALL_LAYOUT, custom_kernel::SGDDenseKernel, float, double) {
+    sgd, intel_gpu, ALL_LAYOUT, custom_kernel::SGDDenseKernel, float, double) {
 }

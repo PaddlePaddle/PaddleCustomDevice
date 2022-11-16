684f4e682d3a0732ef642e0ca9680f3661de1d7b
YuanRisheng <yuanrisheng@baidu.com>
2022-11-16 03:02:51 +0000

    Standardise some C++ API (Part5)

--- backends/npu/kernels/tril_triu_kernel.cc
+++ backends/npu/kernels/tril_triu_kernel.cc
@@ -18,11 +18,11 @@
 namespace custom_kernel {
 
 template <typename T, typename Context>
-void TrilKernel(const Context& dev_ctx,
-                const phi::DenseTensor& x,
-                int diagonal,
-                bool lower,
-                phi::DenseTensor* out) {
+void TrilTriuKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& x,
+                    int diagonal,
+                    bool lower,
+                    phi::DenseTensor* out) {
   const auto* x_data = x.data<T>();
   auto* out_data = dev_ctx.template Alloc<T>(out);
 
@@ -72,8 +72,33 @@ void TrilKernel(const Context& dev_ctx,
   }
 }
 
+template <typename T, typename Context>
+void TrilKernel(const Context& ctx,
+                const phi::DenseTensor& x,
+                int diagonal,
+                phi::DenseTensor* out) {
+  TrilTriuKernel<T, Context>(ctx, x, diagonal, true, out);
+}
+
+template <typename T, typename Context>
+void TriuKernel(const Context& ctx,
+                const phi::DenseTensor& x,
+                int diagonal,
+                phi::DenseTensor* out) {
+  TrilTriuKernel<T, Context>(ctx, x, diagonal, false, out);
+}
+
 }  // namespace custom_kernel
 
+PD_REGISTER_PLUGIN_KERNEL(tril_triu,
+                          npu,
+                          ALL_LAYOUT,
+                          custom_kernel::TrilTriuKernel,
+                          bool,
+                          float,
+                          int,
+                          phi::dtype::float16) {}
+                          
 PD_REGISTER_PLUGIN_KERNEL(tril,
                           npu,
                           ALL_LAYOUT,
@@ -82,3 +107,12 @@ PD_REGISTER_PLUGIN_KERNEL(tril,
                           float,
                           int,
                           phi::dtype::float16) {}
+
+PD_REGISTER_PLUGIN_KERNEL(triu,
+        npu,
+        ALL_LAYOUT,
+        custom_kernel::TriuKernel,
+        bool,
+        float,
+        int,
+        phi::dtype::float16) {}

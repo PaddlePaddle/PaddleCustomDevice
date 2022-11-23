7776877af8dec7202366f25c965afe950dd523ff
USTCKAY <1136780339@qq.com>
2022-11-23 22:21:17 +0800

    [NPU]remove fluid apis accoring to Paddle

--- backends/npu/tests/unittests/test_hard_sigmoid_op_npu.py
+++ backends/npu/tests/unittests/test_hard_sigmoid_op_npu.py
@@ -125,7 +125,7 @@ class TestHardsigmoidAPI(unittest.TestCase):
     def test_fluid_api(self):
         with fluid.program_guard(fluid.Program()):
             x = fluid.data('X', self.x_np.shape, self.x_np.dtype)
-            out = fluid.layers.hard_sigmoid(x)
+            out = paddle.nn.functional.hardsigmoid(x)
             exe = fluid.Executor(self.place)
             res = exe.run(feed={'X': self.x_np}, fetch_list=[out])
         out_ref = ref_hardsigmoid(self.x_np, 0.2, 0.5)
@@ -133,7 +133,7 @@ class TestHardsigmoidAPI(unittest.TestCase):
 
         paddle.disable_static(self.place)
         x = paddle.to_tensor(self.x_np)
-        out = paddle.fluid.layers.hard_sigmoid(x)
+        out = paddle.nn.functional.hardsigmoid(x)
         self.assertTrue(np.allclose(out_ref, out.numpy()))
         paddle.enable_static()
 

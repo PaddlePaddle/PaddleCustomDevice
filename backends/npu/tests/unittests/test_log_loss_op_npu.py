ef79ab0ea14980ad116362e270222dded7d4f4e5
Kai Song <50285351+USTCKAY@users.noreply.github.com>
2022-12-06 18:35:16 -0800

    [NPU]add tril_triu_grad kernel and fix uts (#268)

--- backends/npu/tests/unittests/test_log_loss_op_npu.py
+++ backends/npu/tests/unittests/test_log_loss_op_npu.py
@@ -14,13 +14,11 @@
 
 from __future__ import print_function
 
-import numpy as np
 import unittest
-import sys
 
-from tests.op_test import OpTest
+import numpy as np
 import paddle
-import paddle.fluid as fluid
+from tests.op_test import OpTest
 
 paddle.enable_static()
 
@@ -32,8 +30,8 @@ def sigmoid_array(x):
 class TestLogLossOp(OpTest):
     def setUp(self):
         self.set_npu()
-        self.op_type = 'log_loss'
-        self.place = paddle.CustomPlace('npu', 0)
+        self.op_type = "log_loss"
+        self.place = paddle.CustomPlace("npu", 0)
 
         self.init_dtype()
 
@@ -46,19 +44,20 @@ class TestLogLossOp(OpTest):
         x = np.random.random((samples_num, 1)).astype(self.dtype)
         predicted = sigmoid_array(x)
         labels = np.random.randint(0, 2, (samples_num, 1)).astype(self.dtype)
-        self.inputs = {'Predicted': predicted, 'Labels': labels}
+        self.inputs = {"Predicted": predicted, "Labels": labels}
 
     def set_attrs(self):
         epsilon = 1e-7
-        self.attrs = {'epsilon': epsilon}
+        self.attrs = {"epsilon": epsilon}
 
     def set_outputs(self):
-        epsilon = self.attrs['epsilon']
-        labels = self.inputs['Labels']
-        predicted = self.inputs['Predicted']
-        loss = -labels * np.log(predicted + epsilon) - (
-            1 - labels) * np.log(1 - predicted + epsilon)
-        self.outputs = {'Loss': loss}
+        epsilon = self.attrs["epsilon"]
+        labels = self.inputs["Labels"]
+        predicted = self.inputs["Predicted"]
+        loss = -labels * np.log(predicted + epsilon) - (1 - labels) * np.log(
+            1 - predicted + epsilon
+        )
+        self.outputs = {"Loss": loss}
 
     def set_npu(self):
         self.__class__.use_custom_device = True
@@ -70,37 +69,8 @@ class TestLogLossOp(OpTest):
         self.check_output_with_place(self.place)
 
     def test_check_grad(self):
-        self.check_grad_with_place(self.place, ['Predicted'], 'Loss')
+        self.check_grad_with_place(self.place, ["Predicted"], "Loss")
 
 
-class TestLogLossOpError(unittest.TestCase):
-    def test_errors(self):
-        with fluid.program_guard(fluid.Program()):
-
-            def test_x_type():
-                input_data = np.random.random(100, 1).astype("float32")
-                fluid.layers.log_loss(input_data)
-
-            self.assertRaises(TypeError, test_x_type)
-
-            def test_x_dtype():
-                x2 = fluid.layers.data(name='x2', shape=[100, 1], dtype='int32')
-                fluid.layers.log_loss(x2)
-
-            self.assertRaises(TypeError, test_x_dtype)
-
-            def test_label_type():
-                input_data = np.random.random(100, 1).astype("float32")
-                fluid.layers.log_loss(input_data)
-
-            self.assertRaises(TypeError, test_label_type)
-
-            def test_label_dtype():
-                x2 = fluid.layers.data(name='x2', shape=[100, 1], dtype='int32')
-                fluid.layers.log_loss(x2)
-
-            self.assertRaises(TypeError, test_label_dtype)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

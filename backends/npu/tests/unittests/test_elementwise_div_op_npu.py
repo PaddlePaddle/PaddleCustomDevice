8fac8d75091afa44c9b59b6df0d81253e51e47a6
qili93 <qili93@qq.com>
2022-12-13 15:11:03 +0800

    add two ut fix

--- backends/npu/tests/unittests/test_elementwise_div_op_npu.py
+++ backends/npu/tests/unittests/test_elementwise_div_op_npu.py
@@ -129,12 +129,12 @@ class TestElementwiseDivNet(unittest.TestCase):
             e = paddle.multiply(a, b)
             f = paddle.multiply(c, d)
             f.stop_gradient = True
-            g = fluid.layers.elementwise_div(e, f)
+            g = paddle.divide(e, f)
 
-            fc_1 = fluid.layers.fc(input=g, size=128)
-            prediction = fluid.layers.fc(input=fc_1, size=2, act="softmax")
+            fc_1 = paddle.static.nn.fc(x=g, size=128)
+            prediction = paddle.static.nn.fc(x=fc_1, size=2, activation="softmax")
 
-            cost = fluid.layers.cross_entropy(input=prediction, label=label)
+            cost = paddle.nn.functional.cross_entropy(input=prediction, label=label)
             loss = paddle.mean(cost)
             sgd = fluid.optimizer.SGD(learning_rate=0.01)
             sgd.minimize(loss)

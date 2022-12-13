aafe6f5b028a26b7981fdaa9f1107249c1c96dba
qili93 <qili93@qq.com>
2022-12-13 13:11:26 +0800

    update dockerfile for npu and cpu

---
+++ backends/custom_cpu/tools/dockerfile/build-x86_64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ubuntu18-x86_64-gcc82
+docker build --network=host -f Dockerfile.ubuntu18-x86_64-gcc82 \
+       --build-arg http_proxy=${proxy} \
+       --build-arg https_proxy=${proxy} \
+       --build-arg ftp_proxy=${proxy} \
+       --build-arg no_proxy=bcebos.com \
+       -t registry.baidubce.com/device/paddle-cpu:ubuntu18-x86_64-gcc82 .
+docker push registry.baidubce.com/device/paddle-cpu:ubuntu18-x86_64-gcc82

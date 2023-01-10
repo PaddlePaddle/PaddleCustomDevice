517e6848fc46453fb541c78acd7bedb2bf4b1b44
duanyanhui <duanyanhui@baidu.com>
2023-01-10 15:04:21 +0800

    revert transpose_kernel

--- backends/npu/kernels/transpose_kernel.cc
+++ backends/npu/kernels/transpose_kernel.cc
@@ -30,7 +30,6 @@ void TransposeKernel(const Context& dev_ctx,
       .AddInput(dev_ctx, std::move(axis))
       .AddOutput(*out);
   runner.Run(stream);
-  // VLOG(0) << "test for filter changed kernel";
 }
 
 template <typename T, typename Context>

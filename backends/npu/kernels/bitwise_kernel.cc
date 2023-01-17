06b39aa0ecae442d9a9acb46d33f3a8e7a2d7688
pidack <pcdack@hotmail.com>
2023-01-17 22:37:39 +0800

    fix bitwise series op 0-dim issues

--- backends/npu/kernels/bitwise_kernel.cc
+++ backends/npu/kernels/bitwise_kernel.cc
@@ -16,6 +16,45 @@
 #include "kernels/funcs/npu_op_runner.h"
 
 namespace custom_kernel {
+struct And;
+struct Or;
+struct Xor;
+template <typename P, typename T>
+struct compute {
+  static void run(T var1, T var2, T* var3) {
+    compute<P, T>::run(var1, var2, var3);
+  }
+};
+// bitwise and
+template <typename T>
+struct compute<And, T> {
+  static void run(T var1, T var2, T* var3) { *var3 = var1 & var2; }
+};
+
+// bitwise or
+template <typename T>
+struct compute<Or, T> {
+  static void run(T var1, T var2, T* var3) { *var3 = var1 | var2; }
+};
+
+// bitwise xor
+template <typename T>
+struct compute<Xor, T> {
+  static void run(T var1, T var2, T* var3) { *var3 = var1 ^ var2; }
+};
+
+template <typename T, typename P>
+void FallbackToCPU(const phi::DenseTensor& x,
+                   const phi::DenseTensor& y,
+                   phi::DenseTensor* out) {
+  T val1;
+  T val2;
+  T res;
+  MemCpyD2H(nullptr, &val1, x.data(), sizeof(T));
+  MemCpyD2H(nullptr, &val2, y.data(), sizeof(T));
+  compute<P, T>::run(val1, val2, &res);
+  MemCpyH2D(nullptr, out->data(), &res, sizeof(T));
+}
 
 template <typename T, typename Context>
 void BitwiseAndKernel(const Context& dev_ctx,
@@ -24,26 +63,20 @@ void BitwiseAndKernel(const Context& dev_ctx,
                       phi::DenseTensor* out) {
   dev_ctx.template Alloc<T>(out);
   auto stream = dev_ctx.stream();
-
-  phi::DenseTensor x_tensor(x), y_tensor(y), out_tensor(*out);
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    x_tensor.Resize({1});
-    y_tensor.Resize({1});
-    out_tensor.Resize({1});
+  // TODO(liyuhang06):
+  // Current version CANN bitwise op cann't get accurate result when both input
+  // shape is 1-dim. https://gitee.com/ascend/modelzoo/issues/I6AODW
+  if ((x.dims().size() == 0 && y.dims().size() == 0) ||
+      (x.dims().size() == 1 && y.dims().size() == 1)) {
+    FallbackToCPU<T, And>(x, y, out);
+    return;
   }
-
   if (x.dtype() == phi::DataType::BOOL) {
-    const auto& runner =
-        NpuOpRunner("LogicalAnd", {x_tensor, y_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("LogicalAnd", {x, y}, {*out});
     runner.Run(stream);
   } else {
-    const auto& runner =
-        NpuOpRunner("BitwiseAnd", {x_tensor, y_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("BitwiseAnd", {x, y}, {*out});
     runner.Run(stream);
-  }
-
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    out->Resize({});
   }
 }
 
@@ -54,26 +87,19 @@ void BitwiseOrKernel(const Context& dev_ctx,
                      phi::DenseTensor* out) {
   dev_ctx.template Alloc<T>(out);
   auto stream = dev_ctx.stream();
-
-  phi::DenseTensor x_tensor(x), y_tensor(y), out_tensor(*out);
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    x_tensor.Resize({1});
-    y_tensor.Resize({1});
-    out_tensor.Resize({1});
+  // TODO(liyuhang06):
+  // like BitwiseAnd
+  if ((x.dims().size() == 0 && y.dims().size() == 0) ||
+      (x.dims().size() == 1 && y.dims().size() == 1)) {
+    FallbackToCPU<T, Or>(x, y, out);
+    return;
   }
-
   if (x.dtype() == phi::DataType::BOOL) {
-    const auto& runner =
-        NpuOpRunner("LogicalOr", {x_tensor, y_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("LogicalOr", {x, y}, {*out});
     runner.Run(stream);
   } else {
-    const auto& runner =
-        NpuOpRunner("BitwiseOr", {x_tensor, y_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("BitwiseOr", {x, y}, {*out});
     runner.Run(stream);
-  }
-
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    out->Resize({});
   }
 }
 
@@ -84,29 +110,28 @@ void BitwiseXorKernel(const Context& dev_ctx,
                       phi::DenseTensor* out) {
   dev_ctx.template Alloc<T>(out);
   auto stream = dev_ctx.stream();
-
-  phi::DenseTensor x_tensor(x), y_tensor(y), out_tensor(*out);
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    x_tensor.Resize({1});
-    y_tensor.Resize({1});
-    out_tensor.Resize({1});
+  // TODO(liyuhang06):
+  // like BitwiseAnd
+  if ((x.dims().size() == 0 && y.dims().size() == 0) ||
+      (x.dims().size() == 1 && y.dims().size() == 1)) {
+    FallbackToCPU<T, Xor>(x, y, out);
+    return;
   }
-
   if (x.dtype() == phi::DataType::BOOL) {
     phi::DenseTensor transformed_x, transformed_y, transformed_out;
-    transformed_x.Resize(x_tensor.dims());
-    transformed_y.Resize(y_tensor.dims());
-    transformed_out.Resize(out_tensor.dims());
+    transformed_x.Resize(x.dims());
+    transformed_y.Resize(y.dims());
+    transformed_out.Resize(out->dims());
     dev_ctx.template Alloc<int32_t>(&transformed_x);
     dev_ctx.template Alloc<int32_t>(&transformed_y);
     dev_ctx.template Alloc<int32_t>(&transformed_out);
 
-    const auto& cast_runner1 = NpuOpRunner(
-        "Cast", {x_tensor}, {transformed_x}, {{"dst_type", ACL_INT32}});
+    const auto& cast_runner1 =
+        NpuOpRunner("Cast", {x}, {transformed_x}, {{"dst_type", ACL_INT32}});
     cast_runner1.Run(stream);
 
-    const auto& cast_runner2 = NpuOpRunner(
-        "Cast", {y_tensor}, {transformed_y}, {{"dst_type", ACL_INT32}});
+    const auto& cast_runner2 =
+        NpuOpRunner("Cast", {y}, {transformed_y}, {{"dst_type", ACL_INT32}});
     cast_runner2.Run(stream);
 
     const auto& runner = NpuOpRunner(
@@ -114,16 +139,11 @@ void BitwiseXorKernel(const Context& dev_ctx,
     runner.Run(stream);
 
     const auto& cast_runner3 = NpuOpRunner(
-        "Cast", {transformed_out}, {out_tensor}, {{"dst_type", ACL_BOOL}});
+        "Cast", {transformed_out}, {*out}, {{"dst_type", ACL_BOOL}});
     cast_runner3.Run(stream);
   } else {
-    const auto& runner =
-        NpuOpRunner("BitwiseXor", {x_tensor, y_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("BitwiseXor", {x, y}, {*out});
     runner.Run(stream);
-  }
-
-  if (x.dims().size() == 0 && y.dims().size() == 0) {
-    out->Resize({});
   }
 }
 
@@ -134,22 +154,17 @@ void BitwiseNotKernel(const Context& dev_ctx,
   dev_ctx.template Alloc<T>(out);
   auto stream = dev_ctx.stream();
 
-  phi::DenseTensor x_tensor(x), out_tensor(*out);
+  phi::DenseTensor x_tensor(x);
   if (x.dims().size() == 0) {
     x_tensor.Resize({1});
-    out_tensor.Resize({1});
   }
 
   if (x.dtype() == phi::DataType::BOOL) {
     const auto& runner = NpuOpRunner("LogicalNot", {x}, {*out});
     runner.Run(stream);
   } else {
-    const auto& runner = NpuOpRunner("Invert", {x_tensor}, {out_tensor});
+    const auto& runner = NpuOpRunner("Invert", {x_tensor}, {*out});
     runner.Run(stream);
-  }
-
-  if (x.dims().size() == 0) {
-    out->Resize({});
   }
 }
 

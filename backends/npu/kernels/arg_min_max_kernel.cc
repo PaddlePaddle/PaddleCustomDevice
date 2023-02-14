84da7cfdad6e38b83efd2e6d489bc508507c9902
Kai Song <50285351+USTCKAY@users.noreply.github.com>
2023-02-14 16:59:22 +0800

    [NPU]support host tensor input for TypeAdapter and use it in arg_min (#406)

--- backends/npu/kernels/arg_min_max_kernel.cc
+++ backends/npu/kernels/arg_min_max_kernel.cc
@@ -44,21 +44,31 @@ void ArgMinKernel(const Context& dev_ctx,
     // TODO(songkai05): core dump happend when the dtype of CANN op ArgMin's
     // output is int64, so we compute the int32 result and cast it to int64 when
     // param dtype is 3 temporarily.
-    phi::DenseTensor out_tmp;
-    out_tmp.Resize(out->dims());
-    dev_ctx.template Alloc<int32_t>(&out_tmp);
+    NPUAttributeMap attrs = {{"dtype", dtype}};
 
-    NpuOpRunner runner;
-    runner.SetType("ArgMin")
-        .AddInput(x)
-        .AddInput(dev_ctx, std::vector<int64_t>({axis.to<int64_t>()}))
-        .AddOutput(out_tmp)
-        .AddAttr("dtype", dtype);
-    runner.Run(stream);
+    auto op_runner = [](const std::vector<phi::DenseTensor>& inputs,
+                        const std::vector<phi::DenseTensor>& outputs,
+                        const NPUAttributeMap& attrs,
+                        const phi::CustomContext& dev_ctx,
+                        const auto& host_vecs) {
+      NpuOpRunner runner;
+      runner.SetType("ArgMin")
+          .AddInput(inputs[0])
+          .AddInput(dev_ctx, std::move(host_vecs[0]))
+          .AddOutput(outputs[0])
+          .AddAttrs(attrs);
+      runner.Run(dev_ctx.stream());
+    };
 
-    const auto& cast_runner =
-        NpuOpRunner("Cast", {out_tmp}, {*out}, {{"dst_type", ACL_INT64}});
-    cast_runner.Run(stream);
+    NpuOpRunner::TypeAdapter<int64_t>(
+        {x},
+        {*out},
+        attrs,
+        dev_ctx,
+        op_runner,
+        {x.dtype()},
+        {phi::DataType::INT32},
+        {std::vector<int64_t>({axis.to<int64_t>()})});
   }
 }
 

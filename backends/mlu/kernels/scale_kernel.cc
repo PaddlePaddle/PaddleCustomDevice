939ebd9f2c94490368af91e24599cf1f2c8a47a6
claire199705 <90771635+claire199705@users.noreply.github.com>
2023-02-14 11:24:56 +0800

    [MLU-FIX] fix kernels: scale top_k tril_triu pool2d (#405)

--- backends/mlu/kernels/scale_kernel.cc
+++ backends/mlu/kernels/scale_kernel.cc
@@ -24,78 +24,76 @@ void ScaleKernel(const Context& dev_ctx,
                  float bias,
                  bool bias_after_scale,
                  phi::DenseTensor* out) {
+  // cnnl require input, scale, bias with same type. And all in device side.
+  auto scale = in_scale.to<T>();
+  phi::DenseTensor scale_tensor;
+  scale_tensor.Resize({1});
+  dev_ctx.template Alloc<T>(&scale_tensor);
 
-    // cnnl require input, scale, bias with same type. And all in device side.
-    auto scale = in_scale.to<float>();
-    phi::DenseTensor scale_tensor;
-    scale_tensor.Resize({1});
-    dev_ctx.template Alloc<T>(&scale_tensor);
+  MLUCnnlTensorDesc scale_desc(scale_tensor);
+  MLUCnnl::Fill(dev_ctx,
+                CNNL_POINTER_MODE_HOST,
+                &scale,
+                scale_desc.get(),
+                GetBasePtr(&scale_tensor));
 
-    MLUCnnlTensorDesc scale_desc(scale_tensor);
-    MLUCnnl::Fill(dev_ctx,
-                  CNNL_POINTER_MODE_HOST,
-                  &scale,
-                  scale_desc.get(),
-                  GetBasePtr(&scale_tensor));
+  phi::DenseTensor bias_tensor;
+  bias_tensor.Resize({1});
+  dev_ctx.template Alloc<T>(&bias_tensor);
 
-    phi::DenseTensor bias_tensor;
-    bias_tensor.Resize({1});
-    dev_ctx.template Alloc<T>(&bias_tensor);
+  MLUCnnlTensorDesc bias_desc(bias_tensor);
+  MLUCnnl::Fill(dev_ctx,
+                CNNL_POINTER_MODE_HOST,
+                &bias,
+                bias_desc.get(),
+                GetBasePtr(&bias_tensor));
 
-    MLUCnnlTensorDesc bias_desc(bias_tensor);
-    MLUCnnl::Fill(dev_ctx,
-                  CNNL_POINTER_MODE_HOST,
-                  &bias,
-                  bias_desc.get(),
-                  GetBasePtr(&bias_tensor));
+  dev_ctx.template Alloc<T>(out);
 
-    dev_ctx.template Alloc<T>(out);
+  MLUCnnlTensorDesc input_desc(x);
+  MLUCnnlTensorDesc output_desc(*out);
 
-    MLUCnnlTensorDesc input_desc(x);
-    MLUCnnlTensorDesc output_desc(*out);
+  const int axis = std::max(x.dims().size() - 1, 0);
+  if (bias_after_scale) {
+    MLUCnnl::Scale(dev_ctx,
+                   axis,
+                   input_desc.get(),
+                   GetBasePtr(&x),
+                   scale_desc.get(),
+                   GetBasePtr(&scale_tensor),
+                   bias_desc.get(),
+                   GetBasePtr(&bias_tensor),
+                   output_desc.get(),
+                   GetBasePtr(out));
+  } else {
+    phi::DenseTensor new_bias_tensor;
+    new_bias_tensor.Resize({1});
+    dev_ctx.template Alloc<T>(&new_bias_tensor);
 
-    const int axis = std::max(x.dims().size() - 1, 0);
-    if (bias_after_scale) {
-      MLUCnnl::Scale(dev_ctx,
-                     axis,
-                     input_desc.get(),
-                     GetBasePtr(&x),
-                     scale_desc.get(),
-                     GetBasePtr(&scale_tensor),
-                     bias_desc.get(),
-                     GetBasePtr(&bias_tensor),
-                     output_desc.get(),
-                     GetBasePtr(out));
-    } else {
-      phi::DenseTensor new_bias_tensor;
-      new_bias_tensor.Resize({1});
-      dev_ctx.template Alloc<T>(&new_bias_tensor);
-      
-      MLUCnnlTensorDesc new_bias_desc(new_bias_tensor);
+    MLUCnnlTensorDesc new_bias_desc(new_bias_tensor);
 
-      MLUCnnlOpTensorDesc mul_op_desc(CNNL_OP_TENSOR_MUL,
-                                      ToCnnlDataType(x.dtype()),
-                                      CNNL_NOT_PROPAGATE_NAN);
-      MLUCnnl::OpTensor(dev_ctx,
-                        mul_op_desc.get(),
-                        scale_desc.get(),
-                        GetBasePtr(&scale_tensor),
-                        bias_desc.get(),
-                        GetBasePtr(&bias_tensor),
-                        new_bias_desc.get(),
-                        GetBasePtr(&new_bias_tensor),
-                        ToCnnlDataType(x.dtype()));
-      MLUCnnl::Scale(dev_ctx,
-                     axis,
-                     input_desc.get(),
-                     GetBasePtr(&x),
-                     scale_desc.get(),
-                     GetBasePtr(&scale_tensor),
-                     new_bias_desc.get(),
-                     GetBasePtr(&new_bias_tensor),
-                     output_desc.get(),
-                     GetBasePtr(out));
-    }
+    MLUCnnlOpTensorDesc mul_op_desc(
+        CNNL_OP_TENSOR_MUL, ToCnnlDataType(x.dtype()), CNNL_NOT_PROPAGATE_NAN);
+    MLUCnnl::OpTensor(dev_ctx,
+                      mul_op_desc.get(),
+                      scale_desc.get(),
+                      GetBasePtr(&scale_tensor),
+                      bias_desc.get(),
+                      GetBasePtr(&bias_tensor),
+                      new_bias_desc.get(),
+                      GetBasePtr(&new_bias_tensor),
+                      ToCnnlDataType(x.dtype()));
+    MLUCnnl::Scale(dev_ctx,
+                   axis,
+                   input_desc.get(),
+                   GetBasePtr(&x),
+                   scale_desc.get(),
+                   GetBasePtr(&scale_tensor),
+                   new_bias_desc.get(),
+                   GetBasePtr(&new_bias_tensor),
+                   output_desc.get(),
+                   GetBasePtr(out));
+  }
 }
 
 }  // namespace custom_kernel

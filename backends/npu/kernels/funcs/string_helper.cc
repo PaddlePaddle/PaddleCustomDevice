9c44f78a507022f78f08db34e4522342db8e89a5
duanyanhui <45005871+YanhuiDua@users.noreply.github.com>
2023-04-11 10:16:13 +0800

    [NPU] add tensor utils (#488)

--- backends/npu/kernels/funcs/string_helper.cc
+++ backends/npu/kernels/funcs/string_helper.cc
@@ -100,3 +100,104 @@ std::string GetOpInfoString(std::vector<aclTensorDesc*> descs,
   }
   return ss.str();
 }
+
+template <typename Context, typename T>
+void FormatData(const Context& dev_ctx,
+                const phi::DenseTensor& print_tensor,
+                std::stringstream& log_stream) {
+  int64_t print_size = print_tensor.numel();
+  const T* data = nullptr;
+  phi::DenseTensor cpu_tensor;
+  if (print_tensor.storage_properties_initialized()) {
+    paddle::Tensor x_tensor(std::make_shared<phi::DenseTensor>(print_tensor));
+    auto temp = paddle::npu_identity(x_tensor);
+    auto dense_tensor =
+        std::dynamic_pointer_cast<phi::DenseTensor>(temp.impl());
+    custom_kernel::TensorCopy(
+        dev_ctx, *dense_tensor, true, &cpu_tensor, phi::CPUPlace());
+  } else {
+    custom_kernel::TensorCopy(
+        dev_ctx, print_tensor, true, &cpu_tensor, phi::CPUPlace());
+  }
+  data = cpu_tensor.data<T>();
+  log_stream << "  - data: [";
+  if (print_size > 0) {
+    log_stream << data[0];
+    for (int64_t i = 1; i < print_size; ++i) {
+      log_stream << " " << data[i];
+    }
+  }
+  log_stream << "]" << std::endl;
+}
+
+template void FormatData<phi::CustomContext, float>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+template void FormatData<phi::CustomContext, double>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+template void FormatData<phi::CustomContext, int>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+template void FormatData<phi::CustomContext, int64_t>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+template void FormatData<phi::CustomContext, bool>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+template void FormatData<phi::CustomContext, phi::dtype::float16>(
+    const phi::CustomContext& dev_ctx,
+    const phi::DenseTensor& print_tensor,
+    std::stringstream& log_stream);
+
+template <typename Context>
+std::string GetPDTensorString(const Context& dev_ctx,
+                              const phi::DenseTensor& print_tensor,
+                              const std::string& tensor_name,
+                              const std::string& message) {
+  static std::mutex mutex;
+  std::lock_guard<std::mutex> lock(mutex);
+  std::stringstream log_stream;
+  if (!tensor_name.empty()) {
+    log_stream << "Variable: " << tensor_name << std::endl;
+  }
+
+  if (!message.empty()) {
+    log_stream << "  - message: " << message << std::endl;
+  }
+
+  log_stream << "  - place: " << print_tensor.place() << std::endl;
+  log_stream << "  - shape: " << print_tensor.dims().to_str() << std::endl;
+  log_stream << "  - layout: " << phi::DataLayoutToString(print_tensor.layout())
+             << std::endl;
+
+  auto dtype = print_tensor.dtype();
+  log_stream << "  - dtype: " << dtype << std::endl;
+
+  if (dtype == phi::DataType::FLOAT32) {
+    FormatData<Context, float>(dev_ctx, print_tensor, log_stream);
+  } else if (dtype == phi::DataType::FLOAT64) {
+    FormatData<Context, double>(dev_ctx, print_tensor, log_stream);
+  } else if (dtype == phi::DataType::INT32) {
+    FormatData<Context, int>(dev_ctx, print_tensor, log_stream);
+  } else if (dtype == phi::DataType::INT64) {
+    FormatData<Context, int64_t>(dev_ctx, print_tensor, log_stream);
+  } else if (dtype == phi::DataType::BOOL) {
+    FormatData<Context, bool>(dev_ctx, print_tensor, log_stream);
+  } else if (dtype == phi::DataType::FLOAT16) {
+    FormatData<Context, phi::dtype::float16>(dev_ctx, print_tensor, log_stream);
+  } else {
+    log_stream << "  - data: unprintable type: " << dtype << std::endl;
+  }
+  return log_stream.str();
+}
+
+template std::string GetPDTensorString(const phi::CustomContext& dev_ctx,
+                                       const phi::DenseTensor& print_tensor,
+                                       const std::string& tensor_name,
+                                       const std::string& message);

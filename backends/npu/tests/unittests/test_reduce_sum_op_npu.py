2a0cab344755e4fb15c6458b21ff4c939e7b8cc0
ccsuzzh <1719571694@qq.com>
2023-05-30 21:44:43 +0800

    fix ci

--- backends/npu/tests/unittests/test_reduce_sum_op_npu.py
+++ backends/npu/tests/unittests/test_reduce_sum_op_npu.py
@@ -63,7 +63,7 @@ class TestReduceSum(OpTest):
         self.reduce_all = False
 
     def initTestCase(self):
-        self.shape = (5, 6)
+        self.shape = (5, 6, 10)
         self.axis = (0,)
 
     def test_check_output(self):
@@ -76,6 +76,30 @@ class TestReduceSum(OpTest):
 
 
 class TestReduceSum2(TestReduceSum):
+    def setUp(self):
+        np.random.seed(SEED)
+        self.set_npu()
+        self.init_dtype()
+        self.place = paddle.CustomPlace("npu", 0)
+        self.init_op_type()
+        self.initTestCase()
+
+        self.use_mkldnn = False
+        self.attrs = {
+            "dim": self.axis,
+            "keep_dim": self.keep_dim,
+            "reduce_all": self.reduce_all,
+        }
+        self.inputs = {"X": np.random.random(self.shape).astype(self.dtype)}
+        if self.attrs["reduce_all"]:
+            self.outputs = {"Out": self.inputs["X"].sum().astype(np.int32)}
+        else:
+            self.outputs = {
+                "Out": self.inputs["X"]
+                .sum(axis=self.axis, keepdims=self.attrs["keep_dim"])
+                .astype(np.int32)
+            }
+
     def init_dtype(self):
         self.dtype = np.int32
 

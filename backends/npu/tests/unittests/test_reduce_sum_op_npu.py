4e6e91d4f88c796ceb039d3eeff2819eaa0f31db
Zhenghai Zhang <1719571694@qq.com>
2023-05-31 09:10:03 +0800

    fix ci

--- backends/npu/tests/unittests/test_reduce_sum_op_npu.py
+++ backends/npu/tests/unittests/test_reduce_sum_op_npu.py
@@ -70,43 +70,34 @@ class TestReduceSum(OpTest):
         self.check_output_with_place(self.place)
 
     def test_check_grad(self):
-        if self.dtype == np.float16:
-            return
         self.check_grad_with_place(self.place, ["X"], "Out")
 
 
-class TestReduceSum2(TestReduceSum):
-    def setUp(self):
-        np.random.seed(SEED)
-        self.set_npu()
-        self.init_dtype()
-        self.place = paddle.CustomPlace("npu", 0)
-        self.init_op_type()
-        self.initTestCase()
-
-        self.use_mkldnn = False
-        self.attrs = {
-            "dim": self.axis,
-            "keep_dim": self.keep_dim,
-            "reduce_all": self.reduce_all,
-        }
-        self.inputs = {"X": np.random.random(self.shape).astype(self.dtype)}
-        if self.attrs["reduce_all"]:
-            self.outputs = {"Out": self.inputs["X"].sum().astype(np.int32)}
-        else:
-            self.outputs = {
-                "Out": self.inputs["X"]
-                .sum(axis=self.axis, keepdims=self.attrs["keep_dim"])
-                .astype(np.int32)
-            }
-
-    def init_dtype(self):
-        self.dtype = np.int32
-
-
-class TestReduceSum3(TestReduceSum):
+class TestReduceSumFP16(TestReduceSum):
     def init_dtype(self):
         self.dtype = np.float16
+
+
+class TestReduceSumOp5D(TestReduceSum):
+    def initTestCase(self):
+        self.shape = (1, 2, 5, 6, 10)
+        self.axis = (-1, -2)
+
+
+class TestKeepDimReduce(TestReduceSum):
+    def init_op_type(self):
+        self.op_type = "reduce_sum"
+        self.use_mkldnn = False
+        self.keep_dim = True
+        self.reduce_all = False
+
+
+class TestReduceAll(TestReduceSum):
+    def init_op_type(self):
+        self.op_type = "reduce_sum"
+        self.use_mkldnn = False
+        self.keep_dim = False
+        self.reduce_all = True
 
 
 class TestReduceSumNet(unittest.TestCase):

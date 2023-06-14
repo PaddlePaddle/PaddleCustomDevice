846a728c355898beeb2ead639e0ec7cd68b78a71
USTCKAY <1136780339@qq.com>
2023-06-14 19:50:00 +0800

    [NPU]add inverse and log-fp64 kernel

---
+++ backends/npu/tests/unittests/test_inverse_op_npu.py
@@ -0,0 +1,129 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+from tests.op_test import OpTest
+
+import paddle
+from paddle import fluid
+
+
+class TestInverseOp(OpTest):
+    def set_npu(self):
+        self.__class__.use_custom_device = True
+        self.place = paddle.CustomPlace("npu", 0)
+
+    def config(self):
+        self.matrix_shape = [10, 10]
+        self.dtype = "float64"
+        self.python_api = paddle.tensor.math.inverse
+
+    def setUp(self):
+        self.set_npu()
+        self.op_type = "inverse"
+        self.config()
+
+        np.random.seed(123)
+        mat = np.random.random(self.matrix_shape).astype(self.dtype)
+        inverse = np.linalg.inv(mat)
+
+        self.inputs = {"Input": mat}
+        self.outputs = {"Output": inverse}
+
+    def test_check_output(self):
+        self.check_output_with_place(self.place)
+
+    def test_grad(self):
+        pass
+
+
+class TestInverseOpBatched(TestInverseOp):
+    def config(self):
+        self.matrix_shape = [8, 4, 4]
+        self.dtype = "float64"
+        self.python_api = paddle.tensor.math.inverse
+
+
+class TestInverseOpLarge(TestInverseOp):
+    def config(self):
+        self.matrix_shape = [32, 32]
+        self.dtype = "float64"
+        self.python_api = paddle.tensor.math.inverse
+
+
+class TestInverseOpFP32(TestInverseOp):
+    def config(self):
+        self.matrix_shape = [10, 10]
+        self.dtype = "float32"
+        self.python_api = paddle.tensor.math.inverse
+
+    def test_check_output(self):
+        self.check_output_with_place(self.place, rtol=1e-3, atol=1e-4)
+
+
+class TestInverseOpBatchedFP32(TestInverseOpFP32):
+    def config(self):
+        self.matrix_shape = [8, 4, 4]
+        self.dtype = "float32"
+        self.python_api = paddle.tensor.math.inverse
+
+
+class TestInverseOpLargeFP32(TestInverseOpFP32):
+    def config(self):
+        self.matrix_shape = [32, 32]
+        self.dtype = "float32"
+        self.python_api = paddle.tensor.math.inverse
+
+
+class TestInverseAPI(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(123)
+        self.places = [paddle.CustomPlace("npu", 0)]
+
+    def check_static_result(self, place):
+        paddle.enable_static()
+        with fluid.program_guard(fluid.Program(), fluid.Program()):
+            input = paddle.static.data(name="input", shape=[4, 4], dtype="float64")
+            result = paddle.inverse(x=input)
+            input_np = np.random.random([4, 4]).astype("float64")
+            result_np = np.linalg.inv(input_np)
+
+            exe = fluid.Executor(place)
+            fetches = exe.run(
+                fluid.default_main_program(),
+                feed={"input": input_np},
+                fetch_list=[result],
+            )
+            np.testing.assert_allclose(fetches[0], np.linalg.inv(input_np), rtol=1e-05)
+
+    def test_static(self):
+        for place in self.places:
+            self.check_static_result(place=place)
+
+    def test_dygraph(self):
+        for place in self.places:
+            with fluid.dygraph.guard(place):
+                input_np = np.random.random([4, 4]).astype("float64")
+                input = fluid.dygraph.to_variable(input_np)
+                result = paddle.inverse(input)
+                np.testing.assert_allclose(
+                    result.numpy(), np.linalg.inv(input_np), rtol=1e-05
+                )
+
+
+if __name__ == "__main__":
+    paddle.enable_static()
+    unittest.main()

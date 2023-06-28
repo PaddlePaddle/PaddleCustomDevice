f834195077e8aebff229b24c4e5841d52ee01303
USTCKAY <1136780339@qq.com>
2023-06-28 12:27:00 +0800

    [NPU]delete swish_raw

--- backends/npu/tests/unittests/test_activation_op.py
+++ backends/npu/tests/unittests/test_activation_op.py
@@ -17,7 +17,7 @@ from __future__ import print_function
 import unittest
 
 import numpy as np
-from scipy.special import erf
+from scipy.special import erf, expit
 
 import paddle
 import paddle.fluid as fluid
@@ -1055,6 +1055,89 @@ class TestSin_ZeroDim(TestSin):
         self.shape = []
 
 
+def ref_swish(x):
+    out = x * expit(x)
+    return out
+
+
+class TestSwish(TestActivation):
+    def setUp(self):
+        self.set_npu()
+        self.op_type = "swish"
+        self.init_dtype()
+        self.init_shape()
+
+        np.random.seed(1024)
+        x = np.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        out = ref_swish(x)
+
+        self.inputs = {"X": OpTest.np_dtype_to_fluid_dtype(x)}
+        self.outputs = {"Out": out}
+        self.attrs = {"beta": 1.0}
+
+    def init_shape(self):
+        self.shape = [10, 12]
+
+    def test_check_output(self):
+        self.check_output_with_place(self.place)
+
+    def test_check_grad(self):
+        if self.dtype == np.float16:
+            return
+        self.check_grad_with_place(
+            self.place,
+            ["X"],
+            "Out",
+        )
+
+
+class TestSwish_ZeroDim(TestSwish):
+    def init_shape(self):
+        self.shape = []
+
+
+class TestSwishAPI(unittest.TestCase):
+    # test paddle.nn.Swish, paddle.nn.functional.swish
+    def setUp(self):
+        np.random.seed(1024)
+        self.x_np = np.random.uniform(-1, 1, [10, 12]).astype(np.float64)
+        self.place = paddle.CustomPlace("npu", 0)
+
+    def test_static_api(self):
+        with paddle.fluid.framework._static_guard():
+            with paddle.static.program_guard(paddle.static.Program()):
+                x = paddle.static.data("X", self.x_np.shape, self.x_np.dtype)
+                out1 = F.swish(x)
+                swish = paddle.nn.Swish()
+                out2 = swish(x)
+                exe = paddle.static.Executor(self.place)
+                res = exe.run(feed={"X": self.x_np}, fetch_list=[out1, out2])
+            out_ref = ref_swish(self.x_np)
+            for r in res:
+                np.testing.assert_allclose(out_ref, r, rtol=1e-05)
+
+    def test_dygraph_api(self):
+        paddle.disable_static(self.place)
+        x = paddle.to_tensor(self.x_np)
+        out1 = F.swish(x)
+        swish = paddle.nn.Swish()
+        out2 = swish(x)
+        out_ref = ref_swish(self.x_np)
+        for r in [out1, out2]:
+            np.testing.assert_allclose(out_ref, r.numpy(), rtol=1e-05)
+        paddle.enable_static()
+
+    def test_fluid_api(self):
+        with paddle.fluid.framework._static_guard():
+            with fluid.program_guard(fluid.Program()):
+                x = paddle.static.data("X", self.x_np.shape, self.x_np.dtype)
+                out = paddle.nn.functional.swish(x)
+                exe = fluid.Executor(self.place)
+                res = exe.run(feed={"X": self.x_np}, fetch_list=[out])
+            out_ref = ref_swish(self.x_np)
+            np.testing.assert_allclose(out_ref, res[0], rtol=1e-05)
+
+
 def ref_mish(x, threshold=20.0):
     softplus = np.select([x <= threshold, x > threshold], [np.log(1 + np.exp(x)), x])
     return x * np.tanh(softplus)
@@ -1194,10 +1277,11 @@ create_test_act_fp16_class(TestLog, atol=1e-2)
 create_test_act_fp16_class(TestPow, atol=5e-2)
 create_test_act_fp16_class(TestPow_factor_tensor, atol=5e-2)
 create_test_act_fp16_class(TestFloor, grad_check=False)
+create_test_act_fp16_class(TestSwish)
 create_test_act_fp16_class(TestMish)
 create_test_act_fp16_class(TestRound, grad_check=False)
 
-# TODO(qili93): merge elu, swish ops into activaions
+# TODO(qili93): merge elu op into activaions
 
 if __name__ == "__main__":
     unittest.main()

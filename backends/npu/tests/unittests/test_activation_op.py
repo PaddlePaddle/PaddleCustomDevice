f1317a087c37c73ea5554db79832734c8ef10132
duanyanhui <45005871+YanhuiDua@users.noreply.github.com>
2023-07-07 10:11:48 +0800

    [NPU] add silu/silu_grad kernel (#667)

--- backends/npu/tests/unittests/test_activation_op.py
+++ backends/npu/tests/unittests/test_activation_op.py
@@ -1199,6 +1199,72 @@ class TestSwishAPI(unittest.TestCase):
             np.testing.assert_allclose(out_ref, res[0], rtol=1e-05)
 
 
+class TestSilu(TestActivation):
+    def setUp(self):
+        self.set_npu()
+        self.op_type = "silu"
+        self.init_dtype()
+        self.init_shape()
+
+        np.random.seed(1024)
+        x = np.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        out = x / (np.exp(-x) + 1)
+        self.inputs = {"X": OpTest.np_dtype_to_fluid_dtype(x)}
+        self.outputs = {"Out": out}
+
+    def init_dtype(self):
+        self.dtype = np.float32
+
+    def test_check_output(self):
+        self.check_output_with_place(self.place)
+
+    def test_check_grad(self):
+        # if self.dtype == np.float16:
+        #     return
+        self.check_grad_with_place(
+            self.place,
+            ["X"],
+            "Out",
+        )
+
+
+class TestSilu_ZeroDim(TestSilu):
+    def init_shape(self):
+        self.shape = []
+
+
+class TestSiluAPI(unittest.TestCase):
+    # test paddle.nn.Silu, paddle.nn.functional.silu
+    def setUp(self):
+        np.random.seed(1024)
+        self.x_np = np.random.uniform(-1, 1, [11, 17]).astype("float32")
+        self.place = paddle.CustomPlace("npu", 0)
+
+    def test_static_api(self):
+        with paddle.fluid.framework._static_guard():
+            with paddle.static.program_guard(paddle.static.Program()):
+                x = paddle.static.data("X", [11, 17])
+                out1 = F.silu(x)
+                m = paddle.nn.Silu()
+                out2 = m(x)
+                exe = paddle.static.Executor(self.place)
+                res = exe.run(feed={"X": self.x_np}, fetch_list=[out1, out2])
+            out_ref = self.x_np / (1 + np.exp(-self.x_np))
+            for r in res:
+                np.testing.assert_allclose(out_ref, r, rtol=1e-05)
+
+    def test_dygraph_api(self):
+        paddle.disable_static()
+        x = paddle.to_tensor(self.x_np)
+        out1 = F.silu(x)
+        m = paddle.nn.Silu()
+        out2 = m(x)
+        out_ref = self.x_np / (1 + np.exp(-self.x_np))
+        for r in [out1, out2]:
+            np.testing.assert_allclose(out_ref, r.numpy(), rtol=1e-05)
+        paddle.enable_static()
+
+
 def ref_mish(x, threshold=20.0):
     softplus = np.select([x <= threshold, x > threshold], [np.log(1 + np.exp(x)), x])
     return x * np.tanh(softplus)

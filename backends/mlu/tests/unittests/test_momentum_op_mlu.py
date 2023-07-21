4bbc2c00f2ccd3318eb70cf7de2f7cf4cdf284dc
Chenxiao Niu <ncxinhanzhong@gmail.com>
2023-07-21 10:41:29 +0800

    [MLU] fix uts. (#701)

--- backends/mlu/tests/unittests/test_momentum_op_mlu.py
+++ backends/mlu/tests/unittests/test_momentum_op_mlu.py
@@ -271,11 +271,11 @@ class TestMomentumOpWithDecayAPI(unittest.TestCase):
         out = linear(inp)
         loss = paddle.mean(out)
         # This can be any optimizer supported by dygraph.
-        momentum = paddle.fluid.contrib.optimizer.Momentum(
+        momentum = paddle.optimizer.Momentum(
             learning_rate=0.01,
             momentum=0.9,
-            parameter_list=linear.parameters(),
-            regularization=regularization,
+            parameters=linear.parameters(),
+            weight_decay=regularization,
         )
         momentum.minimize(loss)
 
@@ -295,7 +295,7 @@ class TestMomentumOpWithDecayAPI(unittest.TestCase):
             cost = paddle.nn.functional.square_error_cost(input=y_predict, label=y)
             avg_cost = paddle.mean(cost)
 
-            momentum_optimizer = paddle.fluid.contrib.optimizer.Momentum(
+            momentum_optimizer = paddle.optimizer.Momentum(
                 learning_rate=0.1, momentum=0.9
             )
             momentum_optimizer.minimize(avg_cost)
@@ -423,11 +423,11 @@ class TestMomentumOpVsMomentumOpWithDecayAPI(unittest.TestCase):
             weight_attr=paddle.nn.initializer.Constant(value=2.0),
             bias_attr=paddle.nn.initializer.Constant(value=2.0),
         )
-        momentum_new = paddle.fluid.contrib.optimizer.Momentum(
+        momentum_new = paddle.optimizer.Momentum(
             learning_rate=0.01,
             momentum=0.9,
-            parameter_list=linear_new.parameters(),
-            regularization=paddle.regularizer.L2Decay(coeff=0.1),
+            parameters=linear_new.parameters(),
+            weight_decay=paddle.regularizer.L2Decay(coeff=0.1),
         )
         self.__update_params(momentum=momentum_new, linear=linear_new)
 

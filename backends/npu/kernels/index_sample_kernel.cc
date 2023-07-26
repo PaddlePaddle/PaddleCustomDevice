012726f270940b97668bcb5a99aef3953b5ac2b0
duanyanhui <duanyanhui@baidu.com>
2023-07-26 22:30:00 +0800

    update

--- backends/npu/kernels/index_sample_kernel.cc
+++ backends/npu/kernels/index_sample_kernel.cc
@@ -28,108 +28,115 @@ void IndexSampleGather(const Context& dev_ctx,
   auto index_length = index_dims[1];
   auto stream = dev_ctx.stream();
 
-  phi::DenseTensor tmp_input;
-  phi::DenseTensorMeta input_meta = {input->dtype(),
-                                     phi::make_ddim({input->numel()})};
-  tmp_input.set_meta(input_meta);
-  dev_ctx.template Alloc<T>(&tmp_input);
-  TensorCopy(dev_ctx, *input, false, &tmp_input);
-  tmp_input.Resize({input->numel()});
-  std::vector<phi::DenseTensor> tmp_output;
-  for (auto i = 0; i < batch_size; ++i) {
-    // adds
-    phi::DenseTensor tmp_index, tmp_slice_t, trans_index;
-    tmp_index.Resize(index->dims());
-    tmp_slice_t.Resize(phi::make_ddim({index_length}));
-    trans_index.Resize(index->dims());
-    dev_ctx.template Alloc<int32_t>(&tmp_index);
-    dev_ctx.template Alloc<int32_t>(&tmp_slice_t);
-    dev_ctx.template Alloc<int32_t>(&trans_index);
-    if (index->dtype() == phi::DataType::INT64) {
-      const auto& cast_runner = NpuOpRunner(
-          "Cast", {*index}, {trans_index}, {{"dst_type", ACL_INT32}});
-      cast_runner.Run(stream);
-      const auto& adds_runner =
-          NpuOpRunner("Adds",
-                      {trans_index},
-                      {tmp_index},
-                      {{"value", static_cast<float>(i * input_dims[1])}});
-      adds_runner.Run(stream);
-    } else {
-      const auto& adds_runner =
-          NpuOpRunner("Adds",
-                      {*index},
-                      {tmp_index},
-                      {{"value", static_cast<float>(i * input_dims[1])}});
-      adds_runner.Run(stream);
+  // NPU implementation depends on spliting input in the first dimension.
+  // When the input shape is (100,1), this implementation costs lots of time
+  // to Slice and Gather. It makes more sense when input shape is (1,100).
+  if (input_dims[1] > input_dims[0] * 100) {
+    phi::DenseTensor tmp_input;
+    phi::DenseTensorMeta input_meta = {input->dtype(),
+                                       phi::make_ddim({input->numel()})};
+    tmp_input.set_meta(input_meta);
+    dev_ctx.template Alloc<T>(&tmp_input);
+    TensorCopy(dev_ctx, *input, false, &tmp_input);
+    tmp_input.Resize({input->numel()});
+    std::vector<phi::DenseTensor> tmp_output;
+    for (auto i = 0; i < batch_size; ++i) {
+      // adds
+      phi::DenseTensor tmp_index, tmp_slice_t, trans_index;
+      tmp_index.Resize(index->dims());
+      tmp_slice_t.Resize(phi::make_ddim({index_length}));
+      trans_index.Resize(index->dims());
+      dev_ctx.template Alloc<int32_t>(&tmp_index);
+      dev_ctx.template Alloc<int32_t>(&tmp_slice_t);
+      dev_ctx.template Alloc<int32_t>(&trans_index);
+      if (index->dtype() == phi::DataType::INT64) {
+        const auto& cast_runner = NpuOpRunner(
+            "Cast", {*index}, {trans_index}, {{"dst_type", ACL_INT32}});
+        cast_runner.Run(stream);
+        const auto& adds_runner =
+            NpuOpRunner("Adds",
+                        {trans_index},
+                        {tmp_index},
+                        {{"value", static_cast<float>(i * input_dims[1])}});
+        adds_runner.Run(stream);
+      } else {
+        const auto& adds_runner =
+            NpuOpRunner("Adds",
+                        {*index},
+                        {tmp_index},
+                        {{"value", static_cast<float>(i * input_dims[1])}});
+        adds_runner.Run(stream);
+      }
+      tmp_index.Resize({index->numel()});
+      // slice
+      NpuOpRunner runner1;
+      runner1.SetType("Slice")
+          .AddInput(tmp_index)
+          .AddInput(dev_ctx, std::vector<int32_t>{i * index_length})
+          .AddInput(dev_ctx, std::vector<int32_t>{index_length})
+          .AddOutput(tmp_slice_t)
+          .Run(stream);
+      // gather
+      phi::DenseTensor tmp_out_t;
+      phi::DenseTensorMeta meta = {tmp_input.dtype(),
+                                   phi::make_ddim({index_length})};
+      tmp_out_t.set_meta(meta);
+      if (tmp_input.dtype() == phi::DataType::FLOAT32) {
+        dev_ctx.template Alloc<float>(&tmp_out_t);
+      } else if (tmp_input.dtype() == phi::DataType::INT32) {
+        dev_ctx.template Alloc<int32_t>(&tmp_out_t);
+      } else if (tmp_input.dtype() == phi::DataType::INT64) {
+        dev_ctx.template Alloc<int64_t>(&tmp_out_t);
+      } else if (tmp_input.dtype() == phi::DataType::FLOAT16) {
+        dev_ctx.template Alloc<phi::dtype::float16>(&tmp_out_t);
+      }
+
+      NpuOpRunner gather_runner;
+      gather_runner.SetType("GatherV2")
+          .AddInput(tmp_input)
+          .AddInput(tmp_slice_t)
+          .AddInput(dev_ctx, std::vector<int32_t>{0})
+          .AddOutput(tmp_out_t)
+          .Run(stream);
+      tmp_output.push_back(tmp_out_t);
     }
-    tmp_index.Resize({index->numel()});
-    // slice
-    NpuOpRunner runner1;
-    runner1.SetType("Slice")
-        .AddInput(tmp_index)
-        .AddInput(dev_ctx, std::vector<int32_t>{i * index_length})
-        .AddInput(dev_ctx, std::vector<int32_t>{index_length})
-        .AddOutput(tmp_slice_t)
-        .Run(stream);
-    // gather
-    phi::DenseTensor tmp_out_t;
-    phi::DenseTensorMeta meta = {tmp_input.dtype(),
-                                 phi::make_ddim({index_length})};
-    tmp_out_t.set_meta(meta);
-    if (tmp_input.dtype() == phi::DataType::FLOAT32) {
-      dev_ctx.template Alloc<float>(&tmp_out_t);
-    } else if (tmp_input.dtype() == phi::DataType::INT32) {
-      dev_ctx.template Alloc<int32_t>(&tmp_out_t);
-    } else if (tmp_input.dtype() == phi::DataType::INT64) {
-      dev_ctx.template Alloc<int64_t>(&tmp_out_t);
+
+    // concat
+    std::vector<std::string> names;
+    names.emplace_back("concat_dim");
+    for (size_t i = 0; i < tmp_output.size(); ++i) {
+      names.emplace_back("x" + std::to_string(i));
     }
+    NpuOpRunner concat_runner;
+    concat_runner.SetType("Concat")
+        .AddInput(dev_ctx, std::move(std::vector<int>(1, 0)))
+        .AddInputs(tmp_output)
+        .AddOutput(*out)
+        .AddAttr("N", static_cast<int>(tmp_output.size()))
+        .AddInputNames(names);
+    concat_runner.Run(stream);
+  } else {
+    // CPU implementation for index
+    std::vector<T> gather_index_vec;
+    std::vector<T> index_vec;
+    TensorToVector(dev_ctx, *index, dev_ctx, &index_vec);
+    for (auto i = 0; i < batch_size; ++i) {
+      for (auto j = 0; j < index_length; j++) {
+        gather_index_vec.push_back(i);
+        gather_index_vec.push_back(index_vec[i * index_length + j]);
+      }
+    }
+    phi::DenseTensor gather_index;
+    TensorFromVector(dev_ctx, gather_index_vec, dev_ctx, &gather_index);
+    gather_index.Resize({batch_size, index_length, 2});
 
-    NpuOpRunner gather_runner;
-    gather_runner.SetType("GatherV2")
-        .AddInput(tmp_input)
-        .AddInput(tmp_slice_t)
-        .AddInput(dev_ctx, std::vector<int32_t>{0})
-        .AddOutput(tmp_out_t)
-        .Run(stream);
-    tmp_output.push_back(tmp_out_t);
+    NpuOpRunner runner;
+    runner.SetType("GatherNd")
+        .AddInput(*input)
+        .AddInput(gather_index)
+        .AddOutput(*out);
+    runner.Run(dev_ctx.stream());
   }
-
-  // concat
-  std::vector<std::string> names;
-  names.emplace_back("concat_dim");
-  for (size_t i = 0; i < tmp_output.size(); ++i) {
-    names.emplace_back("x" + std::to_string(i));
-  }
-  NpuOpRunner concat_runner;
-  concat_runner.SetType("Concat")
-      .AddInput(dev_ctx, std::move(std::vector<int>(1, 0)))
-      .AddInputs(tmp_output)
-      .AddOutput(*out)
-      .AddAttr("N", static_cast<int>(tmp_output.size()))
-      .AddInputNames(names);
-  concat_runner.Run(stream);
-
-  // CPU implementation for index
-  // std::vector<T> gather_index_vec;
-  // std::vector<T> index_vec;
-  // TensorToVector(dev_ctx, *index, dev_ctx, &index_vec);
-  // for (auto i = 0; i < batch_size; ++i) {
-  //   for (auto j = 0; j < index_length; j++) {
-  //     gather_index_vec.push_back(i);
-  //     gather_index_vec.push_back(index_vec[i * index_length + j]);
-  //   }
-  // }
-  // phi::DenseTensor gather_index;
-  // TensorFromVector(dev_ctx, gather_index_vec, dev_ctx, &gather_index);
-  // gather_index.Resize({batch_size, index_length, 2});
-
-  // NpuOpRunner runner;
-  // runner.SetType("GatherNd")
-  //     .AddInput(*input)
-  //     .AddInput(gather_index)
-  //     .AddOutput(*out);
-  // runner.Run(dev_ctx.stream());
 }
 
 template <typename T, typename Context>

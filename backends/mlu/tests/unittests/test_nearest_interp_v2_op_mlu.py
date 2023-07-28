ca17f84784fdaad6e0fe7ecd30b34eb96a3e3157
Ruibin Cheung <beinggod@foxmail.com>
2023-07-28 18:40:57 +0800

    [NPU][MLU] fix nearest_interp_v2 case & add 2D sacle tensor case (#727)

--- backends/mlu/tests/unittests/test_nearest_interp_v2_op_mlu.py
+++ backends/mlu/tests/unittests/test_nearest_interp_v2_op_mlu.py
@@ -386,7 +386,12 @@ class TestNearestNeighborInterpDataLayout(TestNearestInterpOp):
 
 
 class TestNearestInterpWithoutCorners(TestNearestInterpOp):
-    def set_align_corners(self):
+    def init_test_case(self):
+        self.interp_method = "nearest"
+        self.input_shape = [3, 2, 7, 5]
+        self.out_h = 64
+        self.out_w = 32
+        self.scale = 2.0
         self.align_corners = False
 
 
@@ -431,6 +436,7 @@ class TestNearestInterpOp_attr_tensor(OpTest):
         self.actual_shape = None
         self.shape_by_1Dtensor = False
         self.scale_by_1Dtensor = False
+        self.scale_by_2Dtensor = False
         self.init_test_case()
         self.op_type = "nearest_interp_v2"
         self.attrs = {
@@ -445,6 +451,10 @@ class TestNearestInterpOp_attr_tensor(OpTest):
             self.inputs["Scale"] = np.array([self.scale]).astype("float32")
             out_h = int(self.input_shape[2] * self.scale)
             out_w = int(self.input_shape[3] * self.scale)
+        elif self.scale_by_2Dtensor:
+            self.inputs["Scale"] = np.array(self.scale).astype("float32")
+            out_h = int(self.input_shape[2] * self.scale[0])
+            out_w = int(self.input_shape[3] * self.scale[1])
         elif self.scale:
             if isinstance(self.scale, float) or isinstance(self.scale, int):
                 if self.scale > 0:
@@ -543,6 +553,19 @@ class TestNearestInterp_attr_tensor_Case3(TestNearestInterpOp_attr_tensor):
         self.out_size = None
         self.align_corners = True
         self.scale_by_1Dtensor = True
+
+
+# scale is a 2-D tensor
+class TestNearestInterp_attr_tensor_Case3(TestNearestInterpOp_attr_tensor):
+    def init_test_case(self):
+        self.interp_method = "nearest"
+        self.input_shape = [3, 2, 32, 16]
+        self.out_h = 64
+        self.out_w = 32
+        self.scale = [2.0, 2.0]
+        self.out_size = None
+        self.align_corners = True
+        self.scale_by_2Dtensor = True
 
 
 class TestNearestInterpException(unittest.TestCase):

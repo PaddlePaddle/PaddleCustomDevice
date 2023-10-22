b00c9eed5834953df945ca850685426d37d517b9
max-niu <57517234+max-niu@users.noreply.github.com>
2023-10-22 14:37:29 +0800

    Update common.py

--- backends/npu/passes/common.py
+++ backends/npu/passes/common.py
@@ -6,7 +6,7 @@ import unittest
 import paddle
 
 from .llama_pass import llama_fuse_attention_dynamic_layer1, llama_fuse_attention_dynamic_layer2, llama_fuse_attention_dynamic_first_parallel_layer, llama_fuse_attention_dynamic_parallel_layer, llama_lmhead, llama_fuse_attention_dynamic_first_parallel_layer_be61d, llama_fuse_attention_dynamic_parallel_layer_be61d
-from .remove_pass import remove_fused_bias_residual_layernorm, remove_rebuild_padding, remove_get_padding_offset, remove_get_token_penalty_multi_scores
+from .remove_pass import remove_fused_bias_residual_layernorm, remove_rebuild_padding, remove_get_padding_offset, remove_get_token_penalty_multi_scores, save_with_output_delay_pass
 
 def setUp():
     for lib in os.listdir(os.getenv("CUSTOM_DEVICE_ROOT")):
@@ -32,6 +32,7 @@ def addPasses(pass_builder, model_type):
         register_pass(pass_builder, "remove_get_padding_offset")
         register_pass(pass_builder, "remove_get_token_penalty_multi_scores")
         register_pass(pass_builder, "llama_lmhead")
+        register_pass(pass_builder, "save_with_output_delay_pass")
         
 
     elif model_type == "llama65B_mp8_dynamic_batch":
@@ -44,6 +45,7 @@ def addPasses(pass_builder, model_type):
         register_pass(pass_builder, "remove_get_padding_offset")
         register_pass(pass_builder, "remove_get_token_penalty_multi_scores")
         register_pass(pass_builder, "llama_lmhead")
+        register_pass(pass_builder, "save_with_output_delay_pass")
 
     else:
         print("NPU pass not support")

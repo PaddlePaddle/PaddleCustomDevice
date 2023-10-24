3104438cee89faeed7e4ff0c1ba1c01b952b5925
max-niu <57517234+max-niu@users.noreply.github.com>
2023-10-24 21:07:44 +0800

    Update common.py

--- backends/npu/passes/common.py
+++ backends/npu/passes/common.py
@@ -6,7 +6,7 @@ import unittest
 import paddle
 
 from .llama_pass import llama_fuse_attention_dynamic_layer1, llama_fuse_attention_dynamic_layer2, llama_fuse_attention_dynamic_first_parallel_layer, llama_fuse_attention_dynamic_parallel_layer, llama_lmhead, llama_fuse_attention_dynamic_first_parallel_layer_be61d, llama_fuse_attention_dynamic_parallel_layer_be61d
-from .remove_pass import remove_fused_bias_residual_layernorm, remove_rebuild_padding, remove_get_padding_offset, remove_get_token_penalty_multi_scores, save_with_output_delay_pass
+from .remove_pass import remove_fused_bias_residual_layernorm, remove_rebuild_padding, remove_get_padding_offset, remove_get_token_penalty_multi_scores, save_with_output_delay_pass_1st, save_with_output_delay_pass_2nd
 
 def setUp():
     for lib in os.listdir(os.getenv("CUSTOM_DEVICE_ROOT")):
@@ -32,7 +32,8 @@ def addPasses(pass_builder, model_type):
         register_pass(pass_builder, "remove_get_padding_offset")
         register_pass(pass_builder, "remove_get_token_penalty_multi_scores")
         register_pass(pass_builder, "llama_lmhead")
-        register_pass(pass_builder, "save_with_output_delay_pass")
+        register_pass(pass_builder, "save_with_output_delay_pass_2nd")
+        register_pass(pass_builder, "save_with_output_delay_pass_1st")
         
 
     elif model_type == "llama65B_mp8_dynamic_batch":
@@ -45,7 +46,8 @@ def addPasses(pass_builder, model_type):
         register_pass(pass_builder, "remove_get_padding_offset")
         register_pass(pass_builder, "remove_get_token_penalty_multi_scores")
         register_pass(pass_builder, "llama_lmhead")
-        register_pass(pass_builder, "save_with_output_delay_pass")
+        register_pass(pass_builder, "save_with_output_delay_pass_2nd")
+        register_pass(pass_builder, "save_with_output_delay_pass_1st")
 
     else:
         print("NPU pass not support")

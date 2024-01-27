6901a8601139a68c394d141f22e353e20f74257d
xuanyuanminzheng <53077335+xuanyuanminzheng@users.noreply.github.com>
2024-01-27 15:41:23 +0800

    Update index_put_kernel.cc

--- backends/npu/kernels/index_put_kernel.cc
+++ backends/npu/kernels/index_put_kernel.cc
@@ -34,15 +34,15 @@ void IndexPutKernel(const Context& dev_ctx,
 
   std::vector<phi::DenseTensor> tensor_list(indices.size());
   for (size_t i = 0; i < indices.size(); i++) {
-      if (indices[i]->dtype() == phi::DataType::INT32){
-          tensor_list[i].Resize(indices[i]->dims());
-          dev_ctx.Alloc(&(tensor_list[i]), phi::DataType::INT64);
-          custom_kernel::CastKernel<T, Context>(
-              dev_ctx, *(indices[i]), phi::DataType::INT64, &(tensor_list[i]));
-      }
-      else{
-          tensor_list[i] = *(indices[i]);
-      }
+    if (indices[i]->dtype() == phi::DataType::INT32){
+        tensor_list[i].Resize(indices[i]->dims());
+        dev_ctx.Alloc(&(tensor_list[i]), phi::DataType::INT64);
+        custom_kernel::CastKernel<T, Context>(
+            dev_ctx, *(indices[i]), phi::DataType::INT64, &(tensor_list[i]));
+    }
+    else{
+        tensor_list[i] = *(indices[i]);
+    }
   }
  
   EXEC_NPU_CMD(
@@ -62,15 +62,15 @@ void IndexPutGradKernel(const Context& dev_ctx,
 
   std::vector<phi::DenseTensor> tensor_list(indices.size());
   for (size_t i = 0; i < indices.size(); i++) {
-      if (indices[i]->dtype() == phi::DataType::INT32){
-          tensor_list[i].Resize(indices[i]->dims());
-          dev_ctx.Alloc(&(tensor_list[i]), phi::DataType::INT64);
-          custom_kernel::CastKernel<T, Context>(
-              dev_ctx, *(indices[i]), phi::DataType::INT64, &(tensor_list[i]));
-      }
-      else{
-          tensor_list[i] = *(indices[i]);
-      }
+    if (indices[i]->dtype() == phi::DataType::INT32){
+        tensor_list[i].Resize(indices[i]->dims());
+        dev_ctx.Alloc(&(tensor_list[i]), phi::DataType::INT64);
+        custom_kernel::CastKernel<T, Context>(
+            dev_ctx, *(indices[i]), phi::DataType::INT64, &(tensor_list[i]));
+    }
+    else{
+        tensor_list[i] = *(indices[i]);
+    }
   }
   
   EXEC_NPU_CMD(

c3f6853e47fa0ff0c519b9e8e8faab2fccf29b54
qili93 <qili93@qq.com>
2024-03-14 17:38:48 +0800

    fix typo

--- backends/custom_cpu/tools/dockerfile/build-image.sh
+++ backends/custom_cpu/tools/dockerfile/build-image.sh
@@ -42,8 +42,8 @@ docker build --network=host -f Dockerfile.ubuntu18.$(uname -m).gcc82 \
   --build-arg https_proxy=${proxy} \
   --build-arg ftp_proxy=${proxy} \
   --build-arg no_proxy=bcebos.com \
-  -t registry.baidubce.com/device/paddle-cpu:ubuntu20-$(uname -m)-gcc84-py310 .
-docker push registry.baidubce.com/device/paddle-cpu:ubuntu20-$(uname -m)-gcc84-py310
+  -t registry.baidubce.com/device/paddle-cpu:ubuntu18-$(uname -m)-gcc84-py310 .
+docker push registry.baidubce.com/device/paddle-cpu:ubuntu18-$(uname -m)-gcc84-py310
 
 # kylinv10.gcc82-py310
 docker build --network=host -f Dockerfile.kylinv10.$(uname -m).gcc82 \

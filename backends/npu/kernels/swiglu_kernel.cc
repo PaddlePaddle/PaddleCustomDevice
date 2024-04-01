105a23316e8e19320290cfcb29c19884bcbe8137
zhenwenqi2024 <zhenwenqi_2022@qq.com>
2024-04-01 11:26:05 +0800

    aclnn call for swiglu backward

--- backends/npu/kernels/swiglu_kernel.cc
+++ backends/npu/kernels/swiglu_kernel.cc
@@ -22,6 +22,12 @@ void ConcatKernel(const Context& dev_ctx,
                   const std::vector<const phi::DenseTensor*>& x,
                   const phi::Scalar& axis_scalar,
                   phi::DenseTensor* out);
+template <typename T, typename Context>
+void SplitKernel(const Context& dev_ctx,
+                 const phi::DenseTensor& x,
+                 const phi::IntArray& num_or_sections,
+                 const phi::Scalar& axis_scalar,
+                 std::vector<phi::DenseTensor*> outs);
 
 template <typename T, typename Context>
 void SwiGluKernel(const Context& dev_ctx,
@@ -69,12 +75,97 @@ void SwiGluKernel(const Context& dev_ctx,
   }
 }
 
-}  // namespace custom_kernel
+template <typename T, typename Context>
+void SwiGluGradKernel(const Context& dev_ctx,
+                      const phi::DenseTensor& x,
+                      const paddle::optional<phi::DenseTensor>& y,
+                      const phi::DenseTensor& dout,
+                      phi::DenseTensor* dx,
+                      phi::DenseTensor* dy) {
+  const auto& dims = x.dims();
+  int64_t axis = -1;
+  if (y) {
+    if (dx || dy) {
+      const auto& y_tensor = y.get();
+      const auto& y_dims = y_tensor.dims();
+      PADDLE_ENFORCE_EQ(y_dims,
+                        dims,
+                        phi::errors::InvalidArgument(
+                            "The shape of Input(Y):[%s] must be equal "
+                            "to the shape of Input(X):[%s].",
+                            y_dims,
+                            dims));
+      // Concatenate x and y in the - 1 dimension
+      phi::DenseTensor concat_xy;
+      auto dst_dims = y_dims;
+      phi::Scalar concat_dim = -1;
+      dst_dims[y_dims.size() - 1] = y_dims[y_dims.size() - 1] * 2;
+      concat_xy.Resize(dst_dims);
+      dev_ctx.template Alloc<T>(&concat_xy);
 
+      phi::DenseTensor dx_temp;
+      dx_temp.Resize(concat_xy.dims());
+      dev_ctx.template Alloc<T>(&dx_temp);
+
+      std::vector<const phi::DenseTensor*> in_tensors{&x, &y_tensor};
+      custom_kernel::ConcatKernel<T, Context>(
+          dev_ctx, in_tensors, &concat_dim, &concat_xy);
+      EXEC_NPU_CMD(aclnnSwiGluGrad, dev_ctx, dout, concat_xy, axis, dx_temp);
+      auto num_or_sections = phi::IntArray({2});
+      auto axis_scalar = phi::Scalar(-1);
+      if (dx && dy) {
+        dev_ctx.template Alloc<T>(dx);
+        dev_ctx.template Alloc<T>(dy);
+        std::vector<phi::DenseTensor*> outs_d = {dx, dy};
+        custom_kernel::SplitKernel<T, Context>(
+            dev_ctx, dx_temp, num_or_sections, axis_scalar, outs_d);
+      } else if (dx) {
+        dev_ctx.template Alloc<T>(dx);
+        phi::DenseTensor dx_fill;
+        dx_fill.Resize(dx->dims());
+        dev_ctx.template Alloc<T>(&dx_fill);
+        std::vector<phi::DenseTensor*> outs_d = {dx, &dx_fill};
+        custom_kernel::SplitKernel<T, Context>(
+            dev_ctx, dx_temp, num_or_sections, axis_scalar, outs_d);
+      } else if (dy) {
+        dev_ctx.template Alloc<T>(dy);
+        phi::DenseTensor dy_fill;
+        dy_fill.Resize(dy->dims());
+        dev_ctx.template Alloc<T>(&dy_fill);
+        std::vector<phi::DenseTensor*> outs_d = {&dy_fill, dy};
+        custom_kernel::SplitKernel<T, Context>(
+            dev_ctx, dx_temp, num_or_sections, axis_scalar, outs_d);
+      }
+    }
+    return;
+  }
+  if (dx) {
+    dev_ctx.template Alloc<T>(dx);
+    auto dims_2d = flatten_to_2d(dims, dims.size() - 1);
+    int64_t n = dims_2d[1];
+    PADDLE_ENFORCE_EQ(n % 2,
+                      0,
+                      phi::errors::InvalidArgument(
+                          "The last dim of Input(X) should be exactly divided "
+                          "by 2 when Input(Y) is None, but got %d",
+                          n));
+    EXEC_NPU_CMD(aclnnSwiGluGrad, dev_ctx, dout, x, axis, *dx);
+  }
+}
+
+}  // namespace custom_kernel
 PD_REGISTER_PLUGIN_KERNEL(swiglu,
                           npu,
                           ALL_LAYOUT,
                           custom_kernel::SwiGluKernel,
+                          float,
+                          phi::dtype::float16,
+                          phi::dtype::bfloat16) {}
+
+PD_REGISTER_PLUGIN_KERNEL(swiglu_grad,
+                          npu,
+                          ALL_LAYOUT,
+                          custom_kernel::SwiGluGradKernel,
                           float,
                           phi::dtype::float16,
                           phi::dtype::bfloat16) {}
---
+++ backends/npu/tests/unittests/test_swiglu_op_npu.py
@@ -0,0 +1,236 @@
+#   Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import numpy as np
+import paddle
+import paddle.nn.functional as F
+import paddle.incubate.nn.functional.swiglu as swigluimpl
+from tests.op_test import convert_float_to_uint16, convert_uint16_to_float
+from npu_utils import check_soc_version
+
+
+def swiglu_naive(x, y=None):
+    if y is None:
+        x, y = paddle.chunk(x, chunks=2, axis=-1)
+    return F.silu(x) * y
+
+
+#  只有X，Y为空
+class TestNPUSwigluFP16OnlyX(unittest.TestCase):
+    def setUp(self):
+        self.npu_place = paddle.CustomPlace("npu", 0)
+        self.cpu_place = paddle.CPUPlace()
+        self.shape = (20, 512)
+        self.init_dtype()
+
+    def init_dtype(self):
+        self.dtype = "float16"
+
+    def check_result(self, golden_res, fused_res):
+        if self.dtype == "float16":
+            rtol = 5e-3
+            atol = 51e-3
+        elif self.dtype == "bfloat16":
+            rtol = 8e-3
+            atol = 8e-3
+        elif self.dtype == "float32":
+            rtol = 1e-3
+            atol = 1e-3
+        else:
+            self.assertTrue(
+                False,
+                msg="Swiglu input dtype only supports bfloat16, \
+                     float16,float32, but got "
+                + self.dtype,
+            )
+        golden_y, golden_dx = golden_res
+        fused_y, fused_dx = fused_res
+        np.testing.assert_allclose(golden_y, fused_y, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_dx, fused_dx, rtol=rtol, atol=atol)
+
+    def golden_swiglu(self, x, y=None):
+        res = swiglu_naive(x)
+        res.backward()
+        if self.dtype == "bfloat16":
+            res = convert_uint16_to_float(res.numpy())
+            dx = convert_uint16_to_float(x.grad.numpy())
+            return res, dx
+        return res.numpy(), x.grad.numpy()
+
+    def fused_swiglu(self, x, y=None):
+        res = swigluimpl(x)
+        res.backward()
+        dx = x.grad
+        if self.dtype == "bfloat16":
+            res = convert_uint16_to_float(res.numpy())
+            dx = convert_uint16_to_float(dx.numpy())
+            return res, dx
+        return res.numpy(), dx.numpy()
+
+    def gen_input(self):
+        x = np.random.randn(self.shape[0], self.shape[1])
+        return x
+
+    @check_soc_version
+    def test_swiglu(self):
+        np_x = self.gen_input()
+        if self.dtype == "bfloat16":
+            np_x = convert_float_to_uint16(np_x)
+        golden_x = paddle.to_tensor(
+            np_x, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+        fused_x = paddle.to_tensor(
+            np_x, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+
+        golden_res = self.golden_swiglu(golden_x)
+        fused_res = self.fused_swiglu(fused_x)
+        self.check_result(golden_res, fused_res)
+
+
+#  X，Y都为空
+class TestNPUSwigluFP16BothXY(TestNPUSwigluFP16OnlyX):
+    def check_result(self, golden_res, fused_res):
+        if self.dtype == "float16":
+            rtol = 5e-3
+            atol = 51e-3
+        elif self.dtype == "bfloat16":
+            rtol = 8e-3
+            atol = 8e-3
+        elif self.dtype == "float32":
+            rtol = 1e-3
+            atol = 1e-3
+        else:
+            self.assertTrue(
+                False,
+                msg="Swiglu input dtype only supports bfloat16, \
+                     float16,float32, but got "
+                + self.dtype,
+            )
+        golden_y, golden_dx, golden_dy = golden_res
+        fused_y, fused_dx, fused_dy = fused_res
+        np.testing.assert_allclose(golden_y, fused_y, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_dx, fused_dx, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_dy, fused_dy, rtol=rtol, atol=atol)
+
+    def gen_input(self):
+        x = np.random.randn(self.shape[0], self.shape[1])
+        y = np.random.randn(self.shape[0], self.shape[1])
+        return x, y
+
+    def golden_swiglu(self, x, y=None):
+        res = swiglu_naive(x, y)
+        res.backward()
+        if self.dtype == "bfloat16":
+            res = convert_uint16_to_float(res.numpy())
+            dx = convert_uint16_to_float(x.grad.numpy())
+            dy = convert_uint16_to_float(y.grad.numpy())
+            return res, dx, dy
+        return res.numpy(), x.grad.numpy(), y.grad.numpy()
+
+    def fused_swiglu(self, x, y):
+        res = swigluimpl(x, y)
+        res.backward()
+        if self.dtype == "bfloat16":
+            res = convert_uint16_to_float(res.numpy())
+            dx = convert_uint16_to_float(x.grad.numpy())
+            dy = convert_uint16_to_float(y.grad.numpy())
+            return res, dx, dy
+        return res.numpy(), x.grad.numpy(), y.grad.numpy()
+
+    @check_soc_version
+    def test_swiglu(self):
+        np_x, np_y = self.gen_input()
+        if self.dtype == "bfloat16":
+            np_x = convert_float_to_uint16(np_x)
+            np_y = convert_float_to_uint16(np_y)
+        golden_x = paddle.to_tensor(
+            np_x, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+        golden_y = paddle.to_tensor(
+            np_y, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+        fused_x = paddle.to_tensor(
+            np_x, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+        fused_y = paddle.to_tensor(
+            np_y, place=self.npu_place, dtype=self.dtype, stop_gradient=False
+        )
+
+        golden_res = self.golden_swiglu(golden_x, golden_y)
+        fused_res = self.fused_swiglu(fused_x, fused_y)
+        self.check_result(golden_res, fused_res)
+
+
+class TestNPUSwigluBF16OnlyX(TestNPUSwigluFP16OnlyX):
+    def init_dtype(self):
+        self.dtype = "bfloat16"
+
+
+class TestNPUSwigluFP32OnlyX(TestNPUSwigluFP16OnlyX):
+    def init_dtype(self):
+        self.dtype = "float32"
+
+
+class TestNPUSwigluBF16BothXY(TestNPUSwigluFP16BothXY):
+    def init_dtype(self):
+        self.dtype = "bfloat16"
+
+
+class TestNPUSwigluFP32BothXY(TestNPUSwigluFP16BothXY):
+    def init_dtype(self):
+        self.dtype = "float32"
+
+
+class TestNPUSwigluFP16OnlyX3D(TestNPUSwigluFP16OnlyX):
+    def setUp(self):
+        self.npu_place = paddle.CustomPlace("npu", 0)
+        self.cpu_place = paddle.CPUPlace()
+        self.shape = (2, 20, 512)
+        self.init_dtype()
+
+
+class TestNPUSwigluFP16BothXY3D(TestNPUSwigluFP16BothXY):
+    def setUp(self):
+        self.npu_place = paddle.CustomPlace("npu", 0)
+        self.cpu_place = paddle.CPUPlace()
+        self.shape = (2, 20, 512)
+        self.init_dtype()
+
+
+class TestNPUSwigluBF16OnlyX3D(TestNPUSwigluFP16OnlyX3D):
+    def init_dtype(self):
+        self.dtype = "bfloat16"
+
+
+class TestNPUSwigluFP32OnlyX3D(TestNPUSwigluFP16OnlyX3D):
+    def init_dtype(self):
+        self.dtype = "float32"
+
+
+class TestNPUSwigluBF16BothXY3D(TestNPUSwigluFP16BothXY3D):
+    def init_dtype(self):
+        self.dtype = "bfloat16"
+
+
+class TestNPUSwigluFP32BothXY3D(TestNPUSwigluFP16BothXY3D):
+    def init_dtype(self):
+        self.dtype = "float32"
+
+
+if __name__ == "__main__":
+    np.random.seed(2024)
+    unittest.main()

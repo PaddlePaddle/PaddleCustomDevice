7d4322708e3a0bf747b0b60c01a8495a9a5fdeb6
wangna11BD <wangna11@baidu.com>
2024-04-09 10:54:08 +0000

    support aclnn for cumsum

--- backends/npu/kernels/cum_kernel.cc
+++ backends/npu/kernels/cum_kernel.cc
@@ -17,6 +17,12 @@
 
 namespace custom_kernel {
 
+template <typename T, typename Context>
+void CastKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                phi::DataType dtype,
+                phi::DenseTensor* out);
+
 using Tensor = phi::DenseTensor;
 
 template <typename Context>
@@ -60,13 +66,13 @@ static void CumsumImp(const Tensor& input,
 }
 
 template <typename T, typename Context>
-void CumsumKernel(const Context& dev_ctx,
-                  const phi::DenseTensor& x,
-                  const phi::Scalar& axis_scalar,
-                  bool flatten,
-                  bool exclusive,
-                  bool reverse,
-                  phi::DenseTensor* out) {
+void AclopCumsumKernel(const Context& dev_ctx,
+                       const phi::DenseTensor& x,
+                       const phi::Scalar& axis_scalar,
+                       bool flatten,
+                       bool exclusive,
+                       bool reverse,
+                       phi::DenseTensor* out) {
   dev_ctx.template Alloc<T>(out);
 
   auto axis = axis_scalar.to<int>();
@@ -89,6 +95,69 @@ void CumsumKernel(const Context& dev_ctx,
     CumsumImp<Context>(new_x, out, attr_input, dev_ctx);
   } else {
     CumsumImp<Context>(x, out, attr_input, dev_ctx);
+  }
+}
+
+template <typename T, typename Context>
+static void CumsumAclnnImp(const Tensor& input,
+                           Tensor* output,
+                           int64_t axis,
+                           const Context& dev_ctx) {
+  if (input.dtype() == phi::DataType::INT64) {
+    Tensor tmp_input;
+    tmp_input.Resize(input.dims());
+
+    custom_kernel::CastKernel<T, Context>(
+        dev_ctx, input, tmp_input.dtype(), &tmp_input);
+
+    Tensor tmp_output;
+    tmp_output.Resize(output->dims());
+    dev_ctx.template Alloc<float>(&tmp_output);
+    auto dst_acl_dtype = ConvertToNpuDtype(tmp_input.dtype());
+
+    EXEC_NPU_CMD(
+        aclnnReduceSum, dev_ctx, tmp_input, axis, dst_acl_dtype, tmp_output);
+
+    custom_kernel::CastKernel<T, Context>(
+        dev_ctx, tmp_output, output->dtype(), output);
+  } else {
+    auto dst_acl_dtype = ConvertToNpuDtype(input.dtype());
+
+    EXEC_NPU_CMD(aclnnReduceSum, dev_ctx, input, axis, dst_acl_dtype, *output);
+  }
+}
+
+template <typename T, typename Context>
+void CumsumKernel(const Context& dev_ctx,
+                  const phi::DenseTensor& x,
+                  const phi::Scalar& axis_scalar,
+                  bool flatten,
+                  bool exclusive,
+                  bool reverse,
+                  phi::DenseTensor* out) {
+  DO_COMPATIBILITY(
+      aclnnCumSum,
+      (custom_kernel::AclopCumsumKernel<T, Context>(
+          dev_ctx, x, axis_scalar, flatten, exclusive, reverse, out)));
+  dev_ctx.template Alloc<T>(out);
+
+  auto axis = axis_scalar.to<int64_t>();
+
+  if (flatten) {
+    PADDLE_ENFORCE_EQ(
+        axis,
+        -1,
+        phi::errors::InvalidArgument(
+            "when flatten is true, attr axis must be default %d, but got %d",
+            -1,
+            axis));
+
+    Tensor new_x(x);
+    new_x.Resize(phi::make_ddim({x.numel()}));
+
+    CumsumAclnnImp<T, Context>(new_x, out, axis, dev_ctx);
+  } else {
+    CumsumAclnnImp<T, Context>(x, out, axis, dev_ctx);
   }
 }
 

ee0fef82c7358510ff2e96c4c0574f1b70c9f1e4
PeiyuLau <liupeiyu@cambricon.com>
2024-04-29 21:46:23 +0800

    [MLU] add fused_rms_norm

---
+++ backends/mlu/custom_op/fused_rms_norm_mlu.cc
@@ -0,0 +1,138 @@
+// Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <vector>
+
+#include "kernels/funcs/mlu_baseop.h"
+#include "kernels/funcs/mlu_funcs.h"
+// #include "kernels/funcs/mlu_op_runner.h"
+#include "paddle/extension.h"
+
+std::vector<std::vector<int64_t>> RmsNormInferShape(
+    const std::vector<int64_t>& x_shape,
+    const std::vector<int64_t>& gamma_shape,
+    float epsilon) {
+  int x_dim = x_shape.size();
+  int rstd_dim = x_dim - gamma_shape.size();
+  std::vector<int64_t> rstd_shape(x_shape.begin(), x_shape.begin() + rstd_dim);
+  rstd_shape.resize(x_dim, 1);
+  return {x_shape, rstd_shape};
+}
+
+std::vector<std::vector<int64_t>> RmsNormGradInferShape(
+    const std::vector<int64_t>& dy_shape,
+    const std::vector<int64_t>& x_shape,
+    const std::vector<int64_t>& rstd_shape,
+    const std::vector<int64_t>& gamma_shape) {
+  return {x_shape, gamma_shape};
+}
+
+std::vector<paddle::Tensor> mlu_rms_norm(const paddle::Tensor& x,
+                                         const paddle::Tensor& gamma,
+                                         float epsilon) {
+  auto dev_ctx = static_cast<const phi::CustomContext*>(
+      paddle::experimental::DeviceContextPool::Instance().Get(x.place()));
+
+  auto x_tensor = static_cast<const phi::DenseTensor*>(x.impl().get());
+  auto gamma_tensor = static_cast<const phi::DenseTensor*>(gamma.impl().get());
+
+  std::shared_ptr<phi::DenseTensor> y_tensor =
+      std::make_shared<phi::DenseTensor>();
+  y_tensor->Resize(x_tensor->dims());
+  dev_ctx->Alloc(y_tensor.get(), gamma_tensor->dtype());
+
+  std::shared_ptr<phi::DenseTensor> rstd_tensor =
+      std::make_shared<phi::DenseTensor>();
+  auto x_shape = phi::vectorize(x_tensor->dims());
+  auto gamma_shape = phi::vectorize(gamma_tensor->dims());
+  int x_dim = x_shape.size();
+  int rstd_dim = x_dim - gamma_shape.size();
+  std::vector<int64_t> rstd_shape_vec(x_shape.begin(),
+                                      x_shape.begin() + rstd_dim);
+  rstd_shape_vec.resize(x_dim, 1);
+  auto rstd_shape = phi::make_ddim(rstd_shape_vec);
+  rstd_tensor->Resize(rstd_shape);
+  dev_ctx->Alloc(rstd_tensor.get(), phi::DataType::FLOAT32);
+  double eps = epsilon;
+
+  MLUCnnlTensorDesc x_desc(x_tensor);
+  MLUCnnlTensorDesc gamma_desc(gamma_tensor);
+  MLUCnnlTensorDesc y_desc(y_tensor);
+  MLUCnnlTensorDesc rstd_desc(rstd_tensor);
+
+  MLUCnnl::RmsNormForward(dev_ctx,
+                          rstd_dim,
+                          x_desc.get(),
+                          GetBasePtr(&x_tensor),
+                          gamma_desc.get(),
+                          GetBasePtr(&gamma_tensor),
+                          nullptr,
+                          eps,
+                          y_desc.get(),
+                          GetBasePtr(y_tensor),
+                          rstd_desc.get(),
+                          GetBasePtr(rstd_tensor));
+
+  return {paddle::Tensor(y_tensor), paddle::Tensor(rstd_tensor)};
+}
+
+// std::vector<paddle::Tensor> npu_rms_norm_grad(const paddle::Tensor& dy,
+//                                               const paddle::Tensor& x,
+//                                               const paddle::Tensor& rstd,
+//                                               const paddle::Tensor& gamma) {
+//   auto dev_ctx = static_cast<const phi::CustomContext*>(
+//       paddle::experimental::DeviceContextPool::Instance().Get(x.place()));
+
+//   auto dy_tensor = static_cast<const phi::DenseTensor*>(dy.impl().get());
+//   auto x_tensor = static_cast<const phi::DenseTensor*>(x.impl().get());
+//   auto rstd_tensor = static_cast<const phi::DenseTensor*>(rstd.impl().get());
+//   auto gamma_tensor = static_cast<const
+//   phi::DenseTensor*>(gamma.impl().get());
+
+//   std::shared_ptr<phi::DenseTensor> dx_tensor =
+//       std::make_shared<phi::DenseTensor>();
+//   dx_tensor->Resize(x_tensor->dims());
+//   dev_ctx->Alloc(dx_tensor.get(), x_tensor->dtype());
+
+//   std::shared_ptr<phi::DenseTensor> dgamma_tensor =
+//       std::make_shared<phi::DenseTensor>();
+//   dgamma_tensor->Resize(gamma_tensor->dims());
+//   dev_ctx->Alloc(dgamma_tensor.get(), phi::DataType::FLOAT32);
+
+//   EXEC_NPU_CMD(aclnnRmsNormGrad,
+//                *dev_ctx,
+//                *dy_tensor,
+//                *x_tensor,
+//                *rstd_tensor,
+//                *gamma_tensor,
+//                *dx_tensor,
+//                *dgamma_tensor);
+//   return {paddle::Tensor(dx_tensor), paddle::Tensor(dgamma_tensor)};
+// }
+
+PD_BUILD_OP(rms_norm_mlu)
+    .Inputs({"x", "gamma"})
+    .Outputs({"y", "rstd"})
+    .Attrs({"epsilon: float"})
+    .SetKernelFn(PD_KERNEL(mlu_rms_norm))
+    .SetInferShapeFn(PD_INFER_SHAPE(
+        RmsNormInferShape));  // neccessary if the op has muti_inputs
+
+// PD_BUILD_GRAD_OP(rms_norm_npu)
+//     .Inputs({paddle::Grad("y"), "x", "rstd", "gamma"})
+//     .Outputs({paddle::Grad("x"), paddle::Grad("gamma")})
+//     .SetKernelFn(PD_KERNEL(npu_rms_norm_grad))
+//     .SetInferShapeFn(PD_INFER_SHAPE(
+//         RmsNormGradInferShape));  // neccessary if the op has muti_inputs
---
+++ backends/mlu/tests/unittests/test_rms_norm_op_mlu_v2.py
@@ -0,0 +1,156 @@
+#   Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+import unittest
+
+import numpy as np
+import paddle
+from paddle.base import core
+from op_test import convert_float_to_uint16
+
+# from npu_utils import check_run_big_shape_test
+
+# from npu_utils import check_soc_version
+
+
+for lib in os.listdir(os.getenv("CUSTOM_DEVICE_ROOT")):
+    print("os.listdir==: ", os.listdir(os.getenv("CUSTOM_DEVICE_ROOT")))
+    if lib.endswith(".so"):
+        paddle.utils.cpp_extension.extension_utils.load_op_meta_info_and_register_op(
+            lib
+        )
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+class TestMLURMSNormFP32(unittest.TestCase):
+    def setUp(self):
+        self.__class__.use_custom_device = True
+        self.place = paddle.CustomPlace("mlu", 0)
+        self.x_shape = (10, 128)
+        self.gamma_shape = (128,)
+        self.eps = 1e-6
+        self.init_dtype()
+
+    def init_dtype(self):
+        self.dtype = "float32"
+
+    def check_result(self, golden_res, fused_res):
+        if self.dtype == "float32":
+            rtol = 1e-04
+            atol = 1e-04
+        elif self.dtype == "float16":
+            rtol = 1e-03
+            atol = 1e-03
+        elif self.dtype == "bfloat16":
+            rtol = 4e-3
+            atol = 4e-3
+        else:
+            self.assertTrue(
+                False,
+                msg="NPURMSNorm input dtype only supports bfloat16, \
+                     float16 and float32, but got "
+                + self.dtype,
+            )
+
+        golden_y, golden_rstd, golden_dx, golden_dgamma = golden_res
+        fused_y, fused_rstd, fused_dx, fused_dgamma = fused_res
+
+        np.testing.assert_allclose(golden_y, fused_y, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_rstd, fused_rstd, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_dx, fused_dx, rtol=rtol, atol=atol)
+        np.testing.assert_allclose(golden_dgamma, fused_dgamma, rtol=rtol, atol=atol)
+
+    def golden_rms_norm(self, x_, gamma, eps):
+        x = x_.cast("float32")
+        var = paddle.mean(paddle.pow(x, 2), axis=-1, keepdim=True)
+        std = paddle.sqrt(var + eps)
+        rstd = 1 / std
+        y = x * rstd
+        y = y.cast(self.dtype)
+        y = y * gamma
+        y.backward()
+        dx = x_.grad
+        dgamma = gamma.grad
+        if self.dtype == "bfloat16":
+            y = y.cast("float32")
+            dx = dx.cast("float32")
+            dgamma = dgamma.cast("float32")
+        return y.numpy(), rstd.numpy(), dx.numpy(), dgamma.numpy()
+
+    def fused_rms_norm(self, x, gamma, eps):
+        y, rstd = core.eager._run_custom_op("rms_norm_mlu", x, gamma, eps)
+        y.backward()
+        dx = x.grad
+        dgamma = gamma.grad
+        if self.dtype == "bfloat16":
+            y = y.cast("float32")
+            dx = dx.cast("float32")
+        return y.numpy(), rstd.numpy(), dx.numpy(), dgamma.numpy()
+
+    def gen_input(self):
+        np_x = np.random.randn(self.x_shape[0], self.x_shape[1]).astype(np.float32)
+        np_gamma = np.random.randn(self.gamma_shape[0]).astype(np.float32)
+        return np_x, np_gamma
+
+    #     @check_soc_version
+    def test_rms_norm(self):
+        np_x, np_gamma = self.gen_input()
+        golden_x = paddle.to_tensor(
+            np_x, place=self.place, dtype=self.dtype, stop_gradient=False
+        )
+        golden_gamma = paddle.to_tensor(
+            np_gamma, place=self.place, dtype=self.dtype, stop_gradient=False
+        )
+        fused_x = paddle.to_tensor(
+            np_x, place=self.place, dtype=self.dtype, stop_gradient=False
+        )
+        fused_gamma = paddle.to_tensor(
+            np_gamma, place=self.place, dtype=self.dtype, stop_gradient=False
+        )
+
+        golden_res = self.golden_rms_norm(golden_x, golden_gamma, self.eps)
+        fused_res = self.fused_rms_norm(fused_x, fused_gamma, self.eps)
+
+        self.check_result(golden_res, fused_res)
+
+
+class TestMLURMSNormFP16(TestMLURMSNormFP32):
+    def init_dtype(self):
+        self.dtype = "float16"
+
+
+# @check_run_big_shape_test()
+# class TestNPURMSNormRank1(TestNPURMSNormFP32):
+#     def init_shape(self):
+#         self.x_shape = (1024, 8192)
+#         self.gamma_shape = (8192,)
+
+
+class TestMLURMSNormBF16(TestMLURMSNormFP32):
+    def init_dtype(self):
+        self.dtype = "bfloat16"
+
+    def gen_input(self):
+        np_x = np.random.randn(self.x_shape[0], self.x_shape[1]).astype(np.float32)
+        np_gamma = np.random.randn(self.gamma_shape[0]).astype(np.float32)
+        np_uint16_x = convert_float_to_uint16(np_x)
+        np_uint16_gamma = convert_float_to_uint16(np_gamma)
+        return np_uint16_x, np_uint16_gamma
+
+
+if __name__ == "__main__":
+    np.random.seed(123)
+    unittest.main()

6025d95dc65337f91ecd89af45125ba1c928afe0
xuanyuanminzheng <53077335+xuanyuanminzheng@users.noreply.github.com>
2024-05-16 14:44:15 +0800

    Update slice_kernel.cc

--- backends/npu/kernels/slice_kernel.cc
+++ backends/npu/kernels/slice_kernel.cc
@@ -151,10 +151,11 @@ void SliceRawKernel(const Context& dev_ctx,
       axes.size(),
       phi::errors::InvalidArgument(
           "The size of starts must be equal to the size of axes."));
-  PADDLE_ENFORCE_EQ(ends.size(),
-                    axes.size(),
-                    phi::errors::InvalidArgument(
-                        "The size of ends must be equal to the size of axes."));
+  PADDLE_ENFORCE_EQ(
+      ends.size(),
+      axes.size(),
+      phi::errors::InvalidArgument(
+          "The size of ends must be equal to the size of axes."));
 
   // Infer output dims
   const auto& in_dims = x.dims();
@@ -164,7 +165,8 @@ void SliceRawKernel(const Context& dev_ctx,
   for (size_t i = 0; i < axes.size(); ++i) {
     // when start == -1 && end == start+1
     if (starts[i] == -1 && ends[i] == 0 && infer_flags[i] == -1) {
-      auto ret = std::find(decrease_axis.begin(), decrease_axis.end(), axes[i]);
+      auto ret =
+          std::find(decrease_axis.begin(), decrease_axis.end(), axes[i]);
       if (ret != decrease_axis.end()) {
         ends[i] = in_dims[axes[i]];
       }
@@ -183,8 +185,14 @@ void SliceRawKernel(const Context& dev_ctx,
   }
 
   dev_ctx.template Alloc<T>(out);
-  EXEC_NPU_CMD(
-      aclnnSliceV2, dev_ctx, x, starts_array, ends_array, axes_t, steps, *out);
+  EXEC_NPU_CMD(aclnnSliceV2,
+               dev_ctx,
+               x,
+               starts_array,
+               ends_array,
+               axes_t,
+               steps,
+               *out);
   out->Resize(out_dims);
 }
 

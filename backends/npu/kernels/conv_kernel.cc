9f32157749957d661ead34d375cbb9aa9bf8f4d9
xuanyuanminzheng <53077335+xuanyuanminzheng@users.noreply.github.com>
2024-07-11 10:46:36 +0800

    Update conv_kernel.cc

--- backends/npu/kernels/conv_kernel.cc
+++ backends/npu/kernels/conv_kernel.cc
@@ -418,7 +418,7 @@ void DepthwiseConv2dGradKernel(const Context& dev_ctx,
   } else {
     phi::DenseTensorMeta filter_grad_meta = {input.dtype(), input.dims()};
     filter_grad_tensor.set_meta(filter_grad_meta);
-    dev_ctx.template Alloc<T>(&filter_grad_tensor); 
+    dev_ctx.template Alloc<T>(&filter_grad_tensor);
   }
 
   if (input_grad) {

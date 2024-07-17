f5dec55fbeff5837826a5dbe7a836b7b70365a00
PeiyuLau <135964669+PeiyuLau@users.noreply.github.com>
2024-07-17 16:27:14 +0800

    [MLU] support gather_nd and transpose int64 dtype (#1343)

--- backends/mlu/kernels/set_value_kernel.cc
+++ backends/mlu/kernels/set_value_kernel.cc
@@ -147,10 +147,8 @@ void SetValueKernel(const Context& dev_ctx,
   index_out.Resize(index_dims);
   dev_ctx.template Alloc<int64_t>(&index_out);
   cnnlScatterRefMode_t mode = CNNL_SCATTERREF_UPDATE;
-  MLUCnnlTensorDesc x_desc(in_temp);
   MLUCnnlTensorDesc indices_desc(index_temp);
   MLUCnnlTensorDesc indices_out_desc(index_out);
-  MLUCnnlTensorDesc updates_desc(val_temp);
   MLUCnnlTensorDesc out_desc(*out);
   MLUCnnl::StridedSlice(dev_ctx,
                         starts_indices,
@@ -165,6 +163,42 @@ void SetValueKernel(const Context& dev_ctx,
       phi::product(slice_dims_for_assign),
       phi::errors::InvalidArgument(
           "OP(set_value) error index indices and value update not match "));
+
+  // when input x and input value's dtype is int64,
+  // cast datadtype to int32 for cnnlScatterRef usage
+  Tensor Ref, Update;
+  Ref.Resize(in_temp.dims());
+  Update.Resize(val_temp.dims());
+
+  if (in_temp.dtype() != DataType::INT64 &&
+      val_temp.dtype() != DataType::INT64 &&
+      in_temp.dtype() != DataType::BOOL && val_temp.dtype() != DataType::BOOL) {
+    Ref = in_temp;
+    Update = val_temp;
+  } else {
+    dev_ctx.template Alloc<int32_t>(&Ref);
+    dev_ctx.template Alloc<int32_t>(&Update);
+    MLUCnnlTensorDesc in_temp_desc(in_temp);
+    MLUCnnlTensorDesc Ref_desc(Ref);
+    MLUCnnlTensorDesc val_temp_desc(val_temp);
+    MLUCnnlTensorDesc Update_desc(Update);
+    cnnlCastDataType_t cast_type = GetCastDataType(x.dtype(), DataType::INT32);
+    MLUCnnl::Cast(dev_ctx,
+                  cast_type,
+                  in_temp_desc.get(),
+                  GetBasePtr(&in_temp),
+                  Ref_desc.get(),
+                  GetBasePtr(&Ref));
+    MLUCnnl::Cast(dev_ctx,
+                  cast_type,
+                  val_temp_desc.get(),
+                  GetBasePtr(&val_temp),
+                  Update_desc.get(),
+                  GetBasePtr(&Update));
+  }
+
+  MLUCnnlTensorDesc Ref_desc(Ref);
+  MLUCnnlTensorDesc Update_desc(Update);
   Tensor index_final;
   index_final = index_out;
   int64_t indices_numel = phi::product(index_dims);
@@ -172,15 +206,34 @@ void SetValueKernel(const Context& dev_ctx,
   index_final.Resize(new_index_dims);
   MLUCnnlTensorDesc indices_final_desc(index_final);
   MLUCnnl::ScatterRefFunctor(dev_ctx,
-                             x_desc.get(),
-                             GetBasePtr(&in_temp),
-                             updates_desc.get(),
-                             GetBasePtr(&val_temp),
+                             Ref_desc.get(),
+                             GetBasePtr(&Ref),
+                             Update_desc.get(),
+                             GetBasePtr(&Update),
                              indices_final_desc.get(),
                              GetBasePtr(&index_final),
                              mode);
-  in_temp.Resize(in_dims);
-  TensorCopy(dev_ctx, in_temp, false, out);
+  Ref.Resize(in_dims);
+
+  // When input x and input value's dtype is int64,
+  // cast ScatterRef output datatype from int32 to int64
+  Tensor in_temp_out;
+  in_temp_out.Resize(Ref.dims());
+  if (x.dtype() != DataType::INT64 && x.dtype() != DataType::BOOL) {
+    in_temp_out = Ref;
+  } else {
+    dev_ctx.template Alloc<T>(&in_temp_out);
+    MLUCnnlTensorDesc in_temp_desc(Ref);
+    MLUCnnlTensorDesc in_temp_out_desc(in_temp_out);
+    cnnlCastDataType_t cast_type = GetCastDataType(Ref.dtype(), x.dtype());
+    MLUCnnl::Cast(dev_ctx,
+                  cast_type,
+                  in_temp_desc.get(),
+                  GetBasePtr(&Ref),
+                  in_temp_out_desc.get(),
+                  GetBasePtr(&in_temp_out));
+  }
+  TensorCopy(dev_ctx, in_temp_out, false, out);
 }
 
 template <typename T, typename Context>
@@ -398,8 +451,13 @@ void SetTensorValueKernel(const Context& dev_ctx,
 
 }  // namespace custom_kernel
 
-PD_REGISTER_PLUGIN_KERNEL(
-    set_value, mlu, ALL_LAYOUT, custom_kernel::SetValueKernel, float, int) {}
+PD_REGISTER_PLUGIN_KERNEL(set_value,
+                          mlu,
+                          ALL_LAYOUT,
+                          custom_kernel::SetValueKernel,
+                          float,
+                          int,
+                          int64_t) {}
 
 PD_REGISTER_PLUGIN_KERNEL(set_value_with_tensor,
                           mlu,

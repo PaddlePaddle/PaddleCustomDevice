6298806a54d13ea6e412a29a6a219d02c6bf9bf8
Zhai Feiyue <feizhai@habana.ai>
2024-08-08 16:00:12 +0800

    add unary kernel

---
+++ backends/intel_hpu/kernels/unary_kernel.cc
@@ -0,0 +1,46 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "funcs.h"
+#include "hpu_operator.h"
+#include "perf_lib_layer_params.h"
+#include "synapse_api.h"
+#include "synapse_common_types.h"
+
+namespace custom_kernel {
+
+template <typename T, typename Context>
+void AbsKernel(const Context& dev_ctx,
+               const phi::DenseTensor& x,
+               phi::DenseTensor* out) {
+  VLOG(6) << "======================================= 111";
+  // allocate memory on device.
+  dev_ctx.template Alloc<T>(out);
+  if (out->numel() == 0) {
+    return;
+  }
+}
+
+}  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(abs,
+                          intel_hpu,
+                          ALL_LAYOUT,
+                          custom_kernel::AbsKernel,
+                          float,
+                          phi::dtype::bfloat16,
+                          phi::dtype::float16,
+                          int64_t) {
+  kernel->InputAt(0).SetDataType(phi::dtype::ToReal(kernel_key.dtype()));
+}

436c2c6f946322a139e519504b4889286697cc00
XieYunshen <1084314248@qq.com>
2024-08-09 15:38:06 +0800

    update

--- cmake/paddle.cmake
+++ cmake/paddle.cmake
@@ -12,8 +12,12 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-find_package(Python ${PYTHON_VERSION} REQUIRED COMPONENTS Interpreter
-                                                          Development EXACT)
+if(NOT PYTHON_VERSION)
+    find_package(Python REQUIRED COMPONENTS Interpreter Development)
+else()
+    find_package(Python ${PYTHON_VERSION} REQUIRED COMPONENTS Interpreter Development EXACT)
+endif()
+
 message(STATUS "Python_EXECUTABLE is ${Python_EXECUTABLE}")
 include_directories(${Python_INCLUDE_DIRS})
 

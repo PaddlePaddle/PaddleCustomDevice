ad79e8a9f22bfd92483a251ac9d2ce4b716dc30a
Zhai Feiyue <feizhai@habana.ai>
2024-08-15 14:07:03 +0800

    add ut

--- backends/intel_hpu/kernels/unary_kernel.cc
+++ backends/intel_hpu/kernels/unary_kernel.cc
@@ -17,33 +17,126 @@
 #include "perf_lib_layer_params.h"
 #include "synapse_api.h"
 #include "synapse_common_types.h"
+#include "utils/utills.h"
 
 namespace custom_kernel {
 
-template <typename T, typename Context>
-void AbsKernel(const Context& dev_ctx,
-               const phi::DenseTensor& x,
-               phi::DenseTensor* out) {
-  VLOG(6) << "======================================= 111";
-  // allocate memory on device.
-  dev_ctx.template Alloc<T>(out);
-  if (out->numel() == 0) {
-    return;
+#define PD_REGISTER_PLUGIN_KERNEL_32bits(OP_NAME, GUID)                     \
+  PD_REGISTER_PLUGIN_KERNEL(                                                \
+      GUID, intel_hpu, ALL_LAYOUT, custom_kernel::OP_NAME##Kernel, float) { \
+    kernel->InputAt(0).SetDataType(phi::dtype::ToReal(kernel_key.dtype())); \
   }
-}
 
+class Unary : public HpuOperator {
+ public:
+  Unary(std::string guid_prefix, std::string node_name)
+      : HpuOperator(guid_prefix), pName_(node_name) {}
+
+  void AddNode(const std::vector<DIMS>& ins,
+               const std::vector<DIMS>& outs,
+               synDataType datatype) {
+    synTensor inputs[ins.size()] = {
+        createTensor(ins[0].size(), datatype, ins[0], true, "x")};
+    synTensor outputs[outs.size()] = {
+        createTensor(outs[0].size(), datatype, outs[0], true, "output")};
+    synStatus status = synNodeCreate(graphHandle_,
+                                     inputs,
+                                     outputs,
+                                     ins.size(),
+                                     outs.size(),
+                                     nullptr,
+                                     0,
+                                     guid_.c_str(),
+                                     pName_.c_str(),
+                                     nullptr,
+                                     nullptr);
+    CHKSTATUS("synNodeCreate add_fwd failed!");
+  }
+  std::string pName_;
+};
+
+#define UNARY_KERNEL(kernel_func, node_name)                                  \
+  template <typename T, typename Context>                                     \
+  void kernel_func##Kernel(const Context& dev_ctx,                            \
+                           const phi::DenseTensor& x,                         \
+                           phi::DenseTensor* out) {                           \
+    dev_ctx.template Alloc<T>(out);                                           \
+    if (out->numel() == 0) {                                                  \
+      return;                                                                 \
+    }                                                                         \
+    std::vector<int64_t> x_dim = phi::vectorize<int64_t>(x.dims());           \
+    std::vector<int64_t> outputs_dim = phi::vectorize<int64_t>(out->dims());  \
+                                                                              \
+    OpCacheOperator op_info;                                                  \
+    op_info.prepareOpInfo<T, nullptr_t>(#node_name "_fwd", {x_dim}, nullptr); \
+    auto recipe = op_info.GetRecipe();                                        \
+                                                                              \
+    if (recipe == nullptr) {                                                  \
+      Unary op(op_info.guid_, #node_name);                                    \
+      op.AddNode({x_dim}, {outputs_dim}, op_info.datatype_);                  \
+      op.Compile();                                                           \
+      op_info.setOp(op);                                                      \
+      recipe = op_info.GetRecipe();                                           \
+    }                                                                         \
+                                                                              \
+    std::map<std::string, uint64_t> tensors;                                  \
+    tensors["x"] = reinterpret_cast<uint64_t>(x.data<T>());                   \
+    tensors["output"] = reinterpret_cast<uint64_t>(out->data<T>());           \
+                                                                              \
+    RecipeRunner runner(recipe);                                              \
+    runner.Run(reinterpret_cast<C_Stream>(dev_ctx.stream()), tensors);        \
+                                                                              \
+    return;                                                                   \
+  }
+
+UNARY_KERNEL(Abs, abs)
+UNARY_KERNEL(Tanh, tanh)
+UNARY_KERNEL(Cos, cos)
+UNARY_KERNEL(Sin, sin)
+UNARY_KERNEL(Tan, tan)
+UNARY_KERNEL(Asin, asin)
+UNARY_KERNEL(Atan, atan)
+UNARY_KERNEL(Acos, acos)
+UNARY_KERNEL(Sinh, sinh)
+UNARY_KERNEL(Cosh, cosh)
+UNARY_KERNEL(Asinh, asinh)
+UNARY_KERNEL(Acosh, acosh)
+UNARY_KERNEL(Atanh, atanh)
+UNARY_KERNEL(Relu, relu)
+UNARY_KERNEL(Silu, silu)
+UNARY_KERNEL(Exp, exp)
+UNARY_KERNEL(Square, square)
+UNARY_KERNEL(Sqrt, sqrt)
+UNARY_KERNEL(Rsqrt, rsqrt)
+UNARY_KERNEL(Softsign, softsign)
+UNARY_KERNEL(Sigmoid, sigmoid)
+UNARY_KERNEL(Log, log)
+UNARY_KERNEL(Negative, neg)
+UNARY_KERNEL(Floor, floor)
+UNARY_KERNEL(Ceil, ceil);
 }  // namespace custom_kernel
 
-// abs
-PD_REGISTER_PLUGIN_KERNEL(abs,
-                          intel_hpu,
-                          ALL_LAYOUT,
-                          custom_kernel::AbsKernel,
-                          float,
-                          phi::dtype::bfloat16,
-                          phi::dtype::float16,
-                          int64_t) {
-  kernel->InputAt(0).SetDataType(phi::dtype::ToReal(kernel_key.dtype()));
-}
-
-// cast
+PD_REGISTER_PLUGIN_KERNEL_32bits(Abs, abs);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Tanh, tanh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Cos, cos);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Sin, sin);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Tan, tan);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Asin, asin);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Atan, atan);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Acos, acos);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Sinh, sinh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Cosh, cosh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Asinh, asinh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Acosh, acosh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Atanh, atanh);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Relu, relu);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Silu, silu);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Exp, exp);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Sqrt, sqrt);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Rsqrt, rsqrt);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Softsign, softsign);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Sigmoid, sigmoid);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Log, log);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Negative, neg);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Floor, floor);
+PD_REGISTER_PLUGIN_KERNEL_32bits(Ceil, ceil);
---
+++ backends/intel_hpu/tests/unittests/test_sigmoid.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import unittest
+
+import numpy as np
+from scipy.special import erf, expit
+
+import paddle
+import paddle.base as base
+import paddle.nn.functional as F
+from tests.op_test import OpTest, convert_float_to_uint16, convert_uint16_to_float
+from tests.utils import static_guard
+
+
+paddle.enable_static()
+SEED = 2021
+
+
+class TestActivation(OpTest):
+    def set_npu(self):
+        self.__class__.use_custom_device = True
+        self.place = paddle.CustomPlace("intel_hpu", 0)
+
+    def setUp(self):
+        self.set_npu()
+        self.op_type = "exp"
+        self.init_dtype()
+        self.init_shape()
+        self.init_kernel_type()
+        self.check_dygraph = True
+        self.python_api = paddle.exp
+
+        np.random.seed(2049)
+        x = np.random.uniform(0.1, 1, self.shape).astype(self.dtype)
+        out = np.exp(x)
+
+        self.inputs = {"X": OpTest.np_dtype_to_base_dtype(x)}
+        self.outputs = {"Out": out}
+
+    def test_check_output(self):
+        check_dygraph = False
+        if hasattr(self, "check_dygraph"):
+            check_dygraph = self.check_dygraph
+        self.check_output_with_place(self.place, check_dygraph=check_dygraph)
+
+    def test_check_grad(self):
+        pass
+
+    def init_dtype(self):
+        self.dtype = np.float32
+
+    def init_shape(self):
+        self.shape = [11, 17]
+
+    def init_kernel_type(self):
+        pass
+
+class TestSigmoid(TestActivation):
+    def setUp(self):
+        self.set_npu()
+        self.op_type = "sigmoid"
+        self.init_dtype()
+        self.init_shape()
+
+        np.random.seed(1024)
+        x = np.random.uniform(-1, 1, self.shape).astype(self.dtype)
+        out = 1 / (1 + np.exp(-x))
+
+        self.inputs = {"X": OpTest.np_dtype_to_base_dtype(x)}
+        self.outputs = {"Out": out}
+
+    def test_check_grad(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()

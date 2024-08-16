6fb01d02ef4ffbfca8acc5efe58c16e47a24c3ab
wangna11BD <wangna11@baidu.com>
2024-08-16 13:28:02 +0000

    fix test

--- backends/npu/tests/unittests/test_cumprod_op_npu.py
+++ backends/npu/tests/unittests/test_cumprod_op_npu.py
@@ -17,7 +17,6 @@ from __future__ import print_function
 import unittest
 import numpy as np
 
-from tests.op_test import OpTest
 import paddle
 import paddle.base.core as core
 import paddle.base as base
@@ -34,21 +33,21 @@ class TestCumprodOp(unittest.TestCase):
         z = np.cumprod(data_np)
         self.assertTrue(np.array_equal(z, y.numpy()))
 
-        y = paddle.cumprod(data, axis=0)
+        y = paddle.cumprod(data, dim=0)
         z = np.cumprod(data_np, axis=0)
         self.assertTrue(np.array_equal(z, y.numpy()))
 
-        y = paddle.cumprod(data, axis=-1)
+        y = paddle.cumprod(data, dim=-1)
         z = np.cumprod(data_np, axis=-1)
         self.assertTrue(np.array_equal(z, y.numpy()))
 
         y = paddle.cumprod(data, dtype="float32")
         self.assertTrue(y.dtype == core.VarDesc.VarType.FP32)
 
-        y = paddle.cumprod(data, dtype=np.int32)
+        y = paddle.cumprod(data, dtype="int32")
         self.assertTrue(y.dtype == core.VarDesc.VarType.INT32)
 
-        y = paddle.cumprod(data, axis=-2)
+        y = paddle.cumprod(data, dim=-2)
         z = np.cumprod(data_np, axis=-2)
         self.assertTrue(np.array_equal(z, y.numpy()))
 
@@ -56,12 +55,12 @@ class TestCumprodOp(unittest.TestCase):
         with base.program_guard(base.Program()):
             data_np = np.random.random((100, 100)).astype(np.float32)
             x = paddle.static.data("X", [100, 100])
-            y = paddle.cumprod(x)
-            y2 = paddle.cumprod(x, axis=0)
-            y3 = paddle.cumprod(x, axis=-1)
-            y4 = paddle.cumprod(x, dtype="float32")
-            y5 = paddle.cumprod(x, dtype=np.int32)
-            y6 = paddle.cumprod(x, axis=-2)
+            y = paddle.cumprod(x, dim=0)
+            y2 = paddle.cumprod(x, dim=1)
+            y3 = paddle.cumprod(x, dim=-1)
+            y4 = paddle.cumprod(x, dim=0, dtype="float32")
+            y5 = paddle.cumprod(x, dim=0, dtype="int32")
+            y6 = paddle.cumprod(x, dim=-2)
 
             place = base.CustomPlace("npu", 0) if use_custom_device else base.CPUPlace()
             exe = base.Executor(place)
@@ -71,9 +70,9 @@ class TestCumprodOp(unittest.TestCase):
                 fetch_list=[y.name, y2.name, y3.name, y4.name, y5.name, y6.name],
             )
 
-            z = np.cumprod(data_np)
-            self.assertTrue(np.allclose(z, out[0]))
             z = np.cumprod(data_np, axis=0)
+            self.assertTrue(np.allclose(z, out[0]))
+            z = np.cumprod(data_np, axis=1)
             self.assertTrue(np.allclose(z, out[1]))
             z = np.cumprod(data_np, axis=-1)
             self.assertTrue(np.allclose(z, out[2]))
@@ -90,200 +89,9 @@ class TestCumprodOp(unittest.TestCase):
     def test_name(self):
         with base.program_guard(base.Program()):
             x = paddle.static.data("x", [3, 4])
-            y = paddle.cumprod(x, name="out")
+            y = paddle.cumprod(x, dim=0, name="out")
             self.assertTrue("out" in y.name)
 
-
-class TestNPUCumprodOp1(OpTest):
-    def setUp(self):
-        self.op_type = "cumprod"
-        self.set_npu()
-        self.init_dtype()
-        self.init_testcase()
-
-    def test_check_output(self):
-        self.check_output_with_place(self.place)
-
-    def set_npu(self):
-        self.__class__.use_custom_device = True
-        self.place = paddle.CustomPlace("npu", 0)
-
-    def init_dtype(self):
-        self.dtype = np.float32
-
-    def init_testcase(self):
-        self.attrs = {"axis": 2}
-        self.inputs = {"X": np.random.random((5, 6, 10)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod(axis=2)}
-
-
-class TestNPUCumprodOp2(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": -1, "reverse": True}
-        self.inputs = {"X": np.random.random((5, 6, 10)).astype(self.dtype)}
-        self.outputs = {
-            "Out": np.flip(np.flip(self.inputs["X"], axis=2).cumprod(axis=2), axis=2)
-        }
-
-
-class TestNPUCumprodOp3(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 1}
-        self.inputs = {"X": np.random.random((5, 6, 10)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod(axis=1)}
-
-
-class TestNPUCumprodOp4(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 0}
-        self.inputs = {"X": np.random.random((5, 6, 10)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod(axis=0)}
-
-
-class TestNPUCumprodOp5(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.inputs = {"X": np.random.random((5, 20)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod(axis=1)}
-
-
-class TestNPUCumprodOp7(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.inputs = {"X": np.random.random((100)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod(axis=0)}
-
-
-class TestNPUCumprodExclusive1(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "exclusive": True}
-        a = np.random.random((4, 5, 65)).astype(self.dtype)
-        self.inputs = {"X": a}
-        self.outputs = {
-            "Out": np.concatenate(
-                (np.zeros((4, 5, 1), dtype=self.dtype), a[:, :, :-1].cumprod(axis=2)),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodExclusive2(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "exclusive": True}
-        a = np.random.random((1, 1, 888)).astype(self.dtype)
-        self.inputs = {"X": a}
-        self.outputs = {
-            "Out": np.concatenate(
-                (np.zeros((1, 1, 1), dtype=self.dtype), a[:, :, :-1].cumprod(axis=2)),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodExclusive3(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "exclusive": True}
-        a = np.random.random((4, 5, 888)).astype(self.dtype)
-        self.inputs = {"X": a}
-        self.outputs = {
-            "Out": np.concatenate(
-                (np.zeros((4, 5, 1), dtype=self.dtype), a[:, :, :-1].cumprod(axis=2)),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodExclusive4(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "exclusive": True}
-        a = np.random.random((1, 1, 3049)).astype(self.dtype)
-        self.inputs = {"X": a}
-        self.outputs = {
-            "Out": np.concatenate(
-                (np.zeros((1, 1, 1), dtype=self.dtype), a[:, :, :-1].cumprod(axis=2)),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodExclusive5(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "exclusive": True}
-        a = np.random.random((4, 5, 3096)).astype(self.dtype)
-        self.inputs = {"X": a}
-        self.outputs = {
-            "Out": np.concatenate(
-                (np.zeros((4, 5, 1), dtype=self.dtype), a[:, :, :-1].cumprod(axis=2)),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodReverseExclusive(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": 2, "reverse": True, "exclusive": True}
-        a = np.random.random((4, 5, 6)).astype(self.dtype)
-        self.inputs = {"X": a}
-        a = np.flip(a, axis=2)
-        self.outputs = {
-            "Out": np.concatenate(
-                (
-                    np.flip(a[:, :, :-1].cumprod(axis=2), axis=2),
-                    np.zeros((4, 5, 1), dtype=self.dtype),
-                ),
-                axis=2,
-            )
-        }
-
-
-class TestNPUCumprodWithFlatten1(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"flatten": True}
-        self.inputs = {"X": np.random.random((5, 6)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod()}
-
-
-class TestNPUCumprodWithFlatten2(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"flatten": True}
-        self.inputs = {"X": np.random.random((5, 6, 10)).astype(self.dtype)}
-        self.outputs = {"Out": self.inputs["X"].cumprod()}
-
-
-# ----------------Cumprod Int64----------------
-class TestNPUCumprodOpInt64(TestNPUCumprodOp1):
-    def init_testcase(self):
-        self.attrs = {"axis": -1, "reverse": True}
-        self.inputs = {
-            "X": np.random.randint(1, 10000, size=(5, 6, 10)).astype(self.dtype)
-        }
-        self.outputs = {
-            "Out": np.flip(np.flip(self.inputs["X"], axis=2).cumprod(axis=2), axis=2)
-        }
-
-
-def create_test_int64(parent):
-    class TestCumprodInt64(parent):
-        def init_dtype(self):
-            self.dtype = np.int64
-
-    cls_name = "{0}_{1}".format(parent.__name__, "Int64")
-    TestCumprodInt64.__name__ = cls_name
-    globals()[cls_name] = TestCumprodInt64
-
-
-create_test_int64(TestNPUCumprodOp1)
-create_test_int64(TestNPUCumprodOp2)
-create_test_int64(TestNPUCumprodOp3)
-create_test_int64(TestNPUCumprodOp4)
-create_test_int64(TestNPUCumprodOp5)
-create_test_int64(TestNPUCumprodOp7)
-create_test_int64(TestNPUCumprodExclusive1)
-create_test_int64(TestNPUCumprodExclusive2)
-create_test_int64(TestNPUCumprodExclusive3)
-create_test_int64(TestNPUCumprodExclusive4)
-create_test_int64(TestNPUCumprodExclusive5)
-create_test_int64(TestNPUCumprodReverseExclusive)
-create_test_int64(TestNPUCumprodWithFlatten1)
-create_test_int64(TestNPUCumprodWithFlatten2)
 
 if __name__ == "__main__":
     unittest.main()

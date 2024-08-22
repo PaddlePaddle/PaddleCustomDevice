8a81b398009d4b0ac75419b8410a12acd962479b
yanfeich <yanfei.cheng@intel.com>
2024-08-22 10:53:50 +0000

    add hpu tril triu kernel

---
+++ backends/intel_hpu/kernels/tril_triu_kernel.cc
@@ -0,0 +1,137 @@
+// Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "funcs.h"
+#include "hpu_operator.h"
+#include "utils/utills.h"
+#include "perf_lib_layer_params.h"
+#include "synapse_api.h"
+#include "synapse_common_types.h"
+
+namespace custom_kernel {
+
+class TrilTriuOperator : public HpuOperator {
+ public:
+  TrilTriuOperator(std::string guid_prefix, std::string node_name)
+    : HpuOperator(guid_prefix), pName_(node_name) {}
+  void AddNode(const std::vector<DIMS>& ins,
+               const std::vector<DIMS>& outs,
+               synDataType datatype,
+               ns_MatrixBandPartKernel::Params params) {
+    assert(ins.size() == 1 && "input size should be 1");
+    assert(outs.size() == 1 && "output size should be 1");
+
+    synTensor inputs[ins.size()] = {
+        createTensor(ins[0].size(), datatype, ins[0], true, "input")};
+    synTensor outputs[outs.size()] = {
+        createTensor(outs[0].size(), datatype, outs[0], true, "output")};
+
+    synStatus status = synNodeCreate(graphHandle_,
+                                     inputs,
+                                     outputs,
+                                     ins.size(),
+                                     outs.size(),
+                                     &params,
+                                     sizeof(params),
+                                     guid_.c_str(),
+                                     pName_.c_str(),
+                                     nullptr,
+                                     nullptr);
+    CHKSTATUS("synNodeCreate matrix_band_part failed!");
+  }
+  std::string pName_;
+};
+
+template <typename T, typename Context>
+void TrilTriuKernel(const Context& dev_ctx,
+                    const phi::DenseTensor& x,
+                    int diagonal,
+                    bool lower,
+                    phi::DenseTensor* out) {
+  // allocate memory on device.
+  dev_ctx.template Alloc<T>(out);
+  if (out->numel() == 0) {
+    return;
+  }
+  std::vector<int64_t> inputs_dim = phi::vectorize<int64_t>(x.dims());
+  std::vector<int64_t> outputs_dim = phi::vectorize<int64_t>(out->dims());
+  ns_MatrixBandPartKernel::Params params;
+  if (lower ) {
+    params.numUpper = diagonal;
+  }
+  else {
+    params.numLower = diagonal;
+  }
+  
+  OpCacheOperator op_info;
+  op_info.prepareOpInfo<T, ns_MatrixBandPartKernel::Params>("matrix_band_part_fwd", {inputs_dim}, &params);
+
+  auto recipe = op_info.GetRecipe();
+  if(recipe == nullptr){
+    // compile
+    TrilTriuOperator op(op_info.guid_, lower ? "Tril_op" : "Triu_op");
+    op.AddNode({inputs_dim}, {outputs_dim}, op_info.datatype_, params);
+    op.Compile();
+    op_info.setOp(op);
+    recipe = op_info.GetRecipe();
+  }
+
+  // runtime
+  std::map<std::string, uint64_t> tensors;
+  tensors["input"] = reinterpret_cast<uint64_t>(x.data<T>());
+  tensors["output"] = reinterpret_cast<uint64_t>(out->data<T>());
+  
+  RecipeRunner runner(recipe); 
+  runner.Run(reinterpret_cast<C_Stream>(dev_ctx.stream()), tensors);
+}
+
+template <typename T, typename Context>
+void TrilKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                int diagonal,
+                phi::DenseTensor* out) {
+  custom_kernel::TrilTriuKernel<T, Context>(dev_ctx, x, diagonal, true, out);
+}
+
+template <typename T, typename Context>
+void TriuKernel(const Context& dev_ctx,
+                const phi::DenseTensor& x,
+                int diagonal,
+                phi::DenseTensor* out) {
+  custom_kernel::TrilTriuKernel<T, Context>(dev_ctx, x, diagonal, false, out);
+}
+
+}  // namespace custom_kernel
+
+
+PD_REGISTER_PLUGIN_KERNEL(tril_triu,
+                          intel_hpu,
+                          ALL_LAYOUT,
+                          custom_kernel::TrilTriuKernel,
+                          float,
+                          phi::dtype::bfloat16) {}
+
+PD_REGISTER_PLUGIN_KERNEL(tril,
+                          intel_hpu,
+                          ALL_LAYOUT,
+                          custom_kernel::TrilKernel,
+                          float,
+                          phi::dtype::bfloat16) {}
+
+PD_REGISTER_PLUGIN_KERNEL(triu,
+                          intel_hpu,
+                          ALL_LAYOUT,
+                          custom_kernel::TriuKernel,
+                          float,
+                          phi::dtype::bfloat16) {}

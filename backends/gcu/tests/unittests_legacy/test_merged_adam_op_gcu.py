fcf8ef8e5bea78601082d476a04d0a35d6b9ceb0
HydrogenSulfate <490868991@qq.com>
2024-10-18 16:11:11 +0800

    Update test_merged_adam_op_gcu.py

--- backends/gcu/tests/unittests_legacy/test_merged_adam_op_gcu.py
+++ backends/gcu/tests/unittests_legacy/test_merged_adam_op_gcu.py
@@ -58,7 +58,7 @@ def run_adam_op(
 
     if not use_merged:
         for i in range(len(param_vars)):
-            _, _, _, _, _, _ = _legacy_C_ops.adam(
+            _, _, _, _, _, *_ = _legacy_C_ops.adam(
                 param_vars[i],
                 grad_vars[i],
                 lr_vars[i],

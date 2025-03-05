5285255d35dec793e9f40ea5ec99eb483d831a16
tianshuo78520a <707759223@qq.com>
2025-03-05 06:17:08 +0000

    update npu dockerfile ;test=document_fix

--- backends/npu/tools/dockerfile/build-image.sh
+++ backends/npu/tools/dockerfile/build-image.sh
@@ -20,12 +20,11 @@ set -ex
 # export CANN_VERSION=8.0.RC1
 # bash build-image.sh ${CANN_VERSION}
 
-CANN_VERSION=${1:-8.0.RC2} # default 8.0.RC2
-SYSTEM=${2:-x86_64} # default 8.0.RC2
+CANN_VERSION=${1:-8.0.0} # default 8.0.0
+SYSTEM=${2:-x86_64} # default x86_64
+NPU_VERSION=${3:-910b} # default 910b
 
-# DOCKER_VERSION=${CANN_VERSION//[^0-9]/} # 801i3
 DOCKER_VERSION=${CANN_VERSION//[^0-9A-Z]/} # 80T13
-#DOCKER_VERSION=${DOCKER_VERSION,,} # lower case
 
 # Download packages from https://www.hiascend.com/software/cann/community first
 if [ ! -f Ascend-cann-toolkit_${CANN_VERSION}_linux-$(uname -m).run ]; then
@@ -33,23 +32,16 @@ if [ ! -f Ascend-cann-toolkit_${CANN_VERSION}_linux-$(uname -m).run ]; then
   exit 1
 fi
 
-# copy file to current directory
-#cp /etc/ascend_install.info ./
-#cp /usr/local/Ascend/driver/version.info ./
-#ubuntu20-npu-base-x86_64-gcc84
-# ubuntu20-$(uname -m)-gcc84-py38
 sed "s#<baseimg>#registry.baidubce.com/device/paddle-cpu:ubuntu20-npu-base-$(uname -m)-gcc84#g" Dockerfile.npu.ubuntu20.gcc84 > Dockerfile.npu.ubuntu20.gcc84.test
 #docker pull registry.baidubce.com/device/paddle-cpu:ubuntu20-npu-base-$(uname -m)-gcc84
 docker build --network=host -f Dockerfile.npu.ubuntu20.gcc84.test \
   --build-arg CANN_VERSION=${CANN_VERSION} \
   --build-arg SYSTEM=${SYSTEM} \
+  --build-arg NPU_VERSION=${NPU_VERSION} \
   --build-arg http_proxy=${proxy} \
   --build-arg https_proxy=${proxy} \
   --build-arg ftp_proxy=${proxy} \
   --build-arg no_proxy=bcebos.com \
-  -t registry.baidubce.com/device/paddle-npu:cann${DOCKER_VERSION}-ubuntu20-npu-base-$(uname -m)-gcc84 .
-docker push registry.baidubce.com/device/paddle-npu:cann${DOCKER_VERSION}-ubuntu20-npu-base-$(uname -m)-gcc84
+  -t ccr-2vdh3abv-pub.cnc.bj.baidubce.com/device/paddle-npu:cann${DOCKER_VERSION}-ubuntu20-npu-${NPU_VERSION}-base-$(uname -m)-gcc84 .
+docker push ccr-2vdh3abv-pub.cnc.bj.baidubce.com/device/paddle-npu:cann${DOCKER_VERSION}-ubuntu20-npu-${NPU_VERSION}-base-$(uname -m)-gcc84
 rm -rf Dockerfile.npu.ubuntu20.gcc84.test
-
-# clean driver info
-#rm -rf *.info

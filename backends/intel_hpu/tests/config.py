f9ad948fee841abeb15bfd1c2b48acf5e9c6fa39
Luo, Focus <focus.luo@intel.com>
2025-04-10 06:18:10 +0000

    [INTEL_HPU] updated stable test cases
    Signed-off-by: Luo, Focus <focus.luo@intel.com>

--- backends/intel_hpu/tests/config.py
+++ backends/intel_hpu/tests/config.py
@@ -19,7 +19,6 @@ skip_case_lst = {}
 # when filter passwdown 'stable' will load this list
 # this list for the unstable test case to skip
 skip_case_lst = [
-    "test_activation_op.py",
-    "test_pow.py",
-    "test_fused_rms_qkv_rope.py",
+    "test_cast.py",
+    "test_gather.py",
 ]

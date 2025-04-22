a9559ab49a03a3795a63ad26e22842e1ea897373
geyuqiang <geyuqiang@baidu.com>
2025-04-22 03:57:30 +0000

    delete useless code

--- cmake/paddle.cmake
+++ cmake/paddle.cmake
@@ -83,13 +83,13 @@ else()
   message(
     STATUS "Run 'git submodule update --init Paddle' in ${REPO_SOURCE_DIR}")
   execute_process(
-   COMMAND git submodule update --init Paddle
-   WORKING_DIRECTORY ${REPO_SOURCE_DIR}
-   RESULT_VARIABLE result_var)
+    COMMAND git submodule update --init Paddle
+    WORKING_DIRECTORY ${REPO_SOURCE_DIR}
+    RESULT_VARIABLE result_var)
   if(NOT result_var EQUAL 0)
-   message(
-     FATAL_ERROR "Failed to get submodule Paddle', please check your network !"
-   )
+    message(
+      FATAL_ERROR "Failed to get submodule Paddle', please check your network !"
+    )
   endif()
 
   get_filename_component(PADDLE_SOURCE_DIR "${REPO_SOURCE_DIR}/Paddle" ABSOLUTE)

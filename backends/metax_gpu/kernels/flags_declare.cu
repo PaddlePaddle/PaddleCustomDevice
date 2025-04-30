b15c63351288e0bb6d94599ed0dd192fe91423f2
geyuqiang <geyuqiang@baidu.com>
2025-04-30 09:10:09 +0000

    fix logsumexp 250430

--- backends/metax_gpu/kernels/flags_declare.cu
+++ backends/metax_gpu/kernels/flags_declare.cu
@@ -1,3 +1,4 @@
 namespace paddle_flags {
 bool FLAGS_cudnn_deterministic = false;
+bool FLAGS_embedding_deterministic = false;
 }
--- backends/metax_gpu/kernels/math_function.cu
+++ backends/metax_gpu/kernels/math_function.cu
@@ -2,9 +2,8 @@
 #include "paddle/phi/common/bfloat16.h"
 #include "paddle/phi/common/data_type.h"
 #include "paddle/phi/common/float16.h"
-#include "paddle/phi/kernels/funcs/math_function_impl.h"
 #include "paddle/phi/kernels/funcs/math_function.h"
-
+#include "paddle/phi/kernels/funcs/math_function_impl.h"
 
 namespace phi {
 namespace funcs {
@@ -22,5 +21,32 @@ template struct SetConstant<phi::CustomContext, int64_t>;
 template struct SetConstant<phi::CustomContext, bool>;
 template struct SetConstant<phi::CustomContext, phi::dtype::complex<float>>;
 template struct SetConstant<phi::CustomContext, phi::dtype::complex<double>>;
-}
-}
+
+#define DEFINE_GPU_TRANS(RANK)                                        \
+  template struct Transpose<phi::CustomContext, bool, RANK>;          \
+  template struct Transpose<phi::CustomContext, unsigned char, RANK>; \
+  template struct Transpose<phi::CustomContext, float, RANK>;         \
+  template struct Transpose<phi::CustomContext, double, RANK>;        \
+  template struct Transpose<phi::CustomContext, float8_e4m3fn, RANK>; \
+  template struct Transpose<phi::CustomContext, float8_e5m2, RANK>;   \
+  template struct Transpose<phi::CustomContext, float16, RANK>;       \
+  template struct Transpose<phi::CustomContext, bfloat16, RANK>;      \
+  template struct Transpose<phi::CustomContext, int8_t, RANK>;        \
+  template struct Transpose<phi::CustomContext, int16_t, RANK>;       \
+  template struct Transpose<phi::CustomContext, int32_t, RANK>;       \
+  template struct Transpose<phi::CustomContext, int64_t, RANK>;       \
+  template struct Transpose<phi::CustomContext,                       \
+                            phi::dtype::complex<float>,               \
+                            RANK>;                                    \
+  template struct Transpose<phi::CustomContext,                       \
+                            phi::dtype::complex<double>,              \
+                            RANK>;
+
+DEFINE_GPU_TRANS(1);
+DEFINE_GPU_TRANS(2);
+DEFINE_GPU_TRANS(3);
+DEFINE_GPU_TRANS(4);
+DEFINE_GPU_TRANS(5);
+DEFINE_GPU_TRANS(6);
+}  // namespace funcs
+}  // namespace phi

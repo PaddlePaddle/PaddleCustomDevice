1df6e7ae92a96817eb0880bef276b1a759b341da
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-05-23 11:10:07 +0800

    update sdaa ci script

--- backends/sdaa/pr_ci_sdaa.sh
+++ backends/sdaa/pr_ci_sdaa.sh
@@ -13,11 +13,13 @@
 # limitations under the License.
 
 # Install paddle whl
-pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
+pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/ --force-reinstall
 
 # make PaddleCustomDevice
 bash compile.sh
 pip install build/dist/*.whl --force-reinstall
+
+python -c "import paddle; paddle.version.show()"
 
 # Test
 cd build/tests/unittests

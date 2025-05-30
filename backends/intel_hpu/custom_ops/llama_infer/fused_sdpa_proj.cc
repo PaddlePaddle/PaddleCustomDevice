075c7b3d8928dcd2909c7a1e8cd72b631e469ed9
Fei Wang <fei.w.wang@intel.com>
2025-05-30 07:02:44 +0000

    [INTEL_HPU] Support FP8 in sdpa proj custom op
    Signed-off-by: Fei Wang <fei.w.wang@intel.com>

--- backends/intel_hpu/custom_ops/llama_infer/fused_sdpa_proj.cc
+++ backends/intel_hpu/custom_ops/llama_infer/fused_sdpa_proj.cc
@@ -18,14 +18,25 @@
 #include "paddle/extension.h"
 #include "utils/utils.h"
 
+#define SDPA_SET_INPUT_AND_FLAGS(ptr, flag_name)  \
+  if (ptr) {                                      \
+    flags |= SdpaFlags_t::SDPA_FLAGS_##flag_name; \
+    ct.Add(ptr);                                  \
+  }
+
 namespace custom_kernel {
+
+struct SDPAParams {
+  bool fp8_sdpa;
+  ns_Sdpa::ParamsV3 params;
+};
 
 class FusedSdpaProj : public HpuOperator {
  public:
   explicit FusedSdpaProj(synDataType dtype)
       : HpuOperator("fused_sdpa_proj_", false), dtype_(dtype) {}
 
-  void AddNode(ConvertTensors& ct, ns_Sdpa::ParamsV2 params) {
+  void AddNode(ConvertTensors& ct, SDPAParams p) {
     auto inputs = ct.GetTensors();
     auto outputs = ct.GetTensors(false);
 
@@ -35,16 +46,23 @@ class FusedSdpaProj : public HpuOperator {
     std::string name_trans = guid_ + "transpose";
     std::string name_reshape = guid_ + "reshape";
     std::string name_gemm = guid_ + "gemm";
+    std::string name_cast = guid_ + "cast";
 
     std::string guid_sdpa = "sdpa_recomp_fwd_";
     std::string guid_reshape = "reshape";
     std::string guid_trans = "transpose";
     std::string guid_gemm = "gemm";
+    std::string guid_cast = "cast_bf16_to_hf8";
 
-    if (dtype_ == syn_type_fp16) {
-      guid_sdpa = guid_sdpa + "f16";
-    } else if (dtype_ == syn_type_bf16) {
-      guid_sdpa = guid_sdpa + "bf16";
+    if (p.fp8_sdpa) {
+      guid_sdpa = guid_sdpa + "hf8";
+      guid_gemm = "fp8_" + guid_gemm + "_bf16";
+    } else {
+      if (dtype_ == syn_type_fp16) {
+        guid_sdpa = guid_sdpa + "f16";
+      } else if (dtype_ == syn_type_bf16) {
+        guid_sdpa = guid_sdpa + "bf16";
+      }
     }
 
     std::vector<synTensor> attn_inputs;
@@ -57,9 +75,24 @@ class FusedSdpaProj : public HpuOperator {
                                          true,
                                          inputs[i].name));
     }
+
+    if (p.fp8_sdpa) {
+      // Seed
+      attn_inputs.push_back(nullptr);
+
+      // Scales
+      for (size_t i = 5; i < inputs.size(); i++) {
+        attn_inputs.push_back(createTensor(inputs[i].dims.size(),
+                                           inputs[i].type,
+                                           inputs[i].dims,
+                                           true,
+                                           inputs[i].name));
+      }
+    }
+
     std::vector<synTensor> attn_outputs;
     auto attn = createTensor(
-        inputs[0].dims.size(), inputs[0].type, inputs[0].dims, false, "attn");
+        inputs[0].dims.size(), syn_type_bf16, inputs[0].dims, false, "attn");
     attn_outputs.push_back(attn);
 
     status = synNodeCreate(graphHandle_,
@@ -67,8 +100,8 @@ class FusedSdpaProj : public HpuOperator {
                            attn_outputs.data(),
                            attn_inputs.size(),
                            attn_outputs.size(),
-                           &params,
-                           sizeof(params),
+                           &p.params,
+                           sizeof(p.params),
                            guid_sdpa.c_str(),
                            name_sdpa.c_str(),
                            nullptr,
@@ -76,6 +109,33 @@ class FusedSdpaProj : public HpuOperator {
     PD_CHECK(status == synSuccess,
              "[RUNTIME] FusedSdpaProjKernel synNodeCreate () failed = ",
              status);
+
+    std::vector<synTensor> attn_out_cast;
+    if (p.fp8_sdpa) {
+      // If fp8_sdpa, we need to cast the output of SDPA
+      // to fp8.
+      auto att_c = createTensor(inputs[0].dims.size(),
+                                inputs[0].type,
+                                inputs[0].dims,
+                                false,
+                                "cast_fp8");
+      attn_out_cast.push_back(att_c);
+
+      status = synNodeCreate(graphHandle_,
+                             attn_outputs.data(),
+                             attn_out_cast.data(),
+                             1,
+                             1,
+                             nullptr,
+                             0,
+                             guid_cast.c_str(),
+                             name_cast.c_str(),
+                             nullptr,
+                             nullptr);
+      PD_CHECK(status == synSuccess,
+               "[RUNTIME] FusedSdpaProjKernel synNodeCreate () failed = ",
+               status);
+    }
 
     std::vector<int64_t> q_dims = std::vector<int64_t>(inputs[0].dims);
     std::vector<int64_t> qt_dims(q_dims.cbegin(), q_dims.cend());
@@ -96,17 +156,18 @@ class FusedSdpaProj : public HpuOperator {
         createTensor(qt_dims.size(), inputs[0].type, qt_dims, false, "attn_t");
     attn_out_transpose.push_back(attn_t);
 
-    status = synNodeCreate(graphHandle_,
-                           attn_outputs.data(),
-                           attn_out_transpose.data(),
-                           1,
-                           1,
-                           &trans_params,
-                           sizeof(trans_params),
-                           guid_trans.c_str(),
-                           name_trans.c_str(),
-                           nullptr,
-                           nullptr);
+    status =
+        synNodeCreate(graphHandle_,
+                      p.fp8_sdpa ? attn_out_cast.data() : attn_outputs.data(),
+                      attn_out_transpose.data(),
+                      1,
+                      1,
+                      &trans_params,
+                      sizeof(trans_params),
+                      guid_trans.c_str(),
+                      name_trans.c_str(),
+                      nullptr,
+                      nullptr);
     PD_CHECK(status == synSuccess,
              "[RUNTIME] FusedSdpaProjKernel synNodeCreate () failed = ",
              status);
@@ -143,6 +204,7 @@ class FusedSdpaProj : public HpuOperator {
                                       inputs[4].dims,
                                       true,
                                       inputs[4].name));
+
     std::vector<synTensor> mul_outputs;
     mul_outputs.push_back(createTensor(outputs[0].dims.size(),
                                        outputs[0].type,
@@ -180,7 +242,13 @@ void FusedSdpaProjKernel(const Context& dev_ctx,
                          const phi::DenseTensor& attn_mask,
                          const phi::DenseTensor& linear_weights,
                          phi::DenseTensor* out_linear,
-                         const phi::Scalar& scaling_factor) {
+                         const phi::Scalar& scaling_factor,
+                         const paddle::optional<phi::DenseTensor>& d_scale_q,
+                         const paddle::optional<phi::DenseTensor>& d_scale_k,
+                         const paddle::optional<phi::DenseTensor>& d_scale_v,
+                         const paddle::optional<phi::DenseTensor>& q_scale_s,
+                         const paddle::optional<phi::DenseTensor>& q_scale_o,
+                         const paddle::optional<phi::DenseTensor>& d_scale_s) {
   ConvertTensors ct;
   ct.Add(query_states);
   ct.Add(key_states);
@@ -188,6 +256,16 @@ void FusedSdpaProjKernel(const Context& dev_ctx,
   ct.Add(value_states);
   ct.Add(attn_mask);
   ct.Add(linear_weights);
+
+  unsigned int flags = 0;
+
+  SDPA_SET_INPUT_AND_FLAGS(d_scale_q.get_ptr(), D_SCALE_Q)
+  SDPA_SET_INPUT_AND_FLAGS(d_scale_k.get_ptr(), D_SCALE_K)
+  SDPA_SET_INPUT_AND_FLAGS(d_scale_v.get_ptr(), D_SCALE_V)
+  SDPA_SET_INPUT_AND_FLAGS(q_scale_s.get_ptr(), Q_SCALE_S)
+  SDPA_SET_INPUT_AND_FLAGS(q_scale_o.get_ptr(), Q_SCALE_O)
+  SDPA_SET_INPUT_AND_FLAGS(d_scale_s.get_ptr(), D_SCALE_S)
+
   ct.Add(out_linear, false);
   std::vector<DIMS> out_dims = ct.GetDims(false);
   in_out_dims.insert(in_out_dims.end(), out_dims.begin(), out_dims.end());
@@ -195,19 +273,21 @@ void FusedSdpaProjKernel(const Context& dev_ctx,
   OpCacheOperator op_info;
   op_info.prepareOpInfo<T, nullptr_t>("fused_sdpa_proj_", in_out_dims, nullptr);
   auto recipe = op_info.GetRecipe();
+  phi::DataType dtype = query_states.dtype();
 
   if (recipe == nullptr) {
-    ns_Sdpa::ParamsV2 params;
-    memset(reinterpret_cast<void*>(&params), 0x00, sizeof(ns_Sdpa::ParamsV2));
-    params.scale = scaling_factor.to<float>();
-    params.is_causal = false;  // true;
-    params.dropout.ratio = 0.0;
-    params.dropout.disableMaskOut = false;
-    params.is_inference = true;
-    params.softmax_mode = SDPA_DEFAULT_SOFTMAX;
+    SDPAParams p = {};
+    p.params.scale = scaling_factor.to<float>();
+    p.params.is_causal = false;  // true;
+    p.params.dropout.ratio = 0.0;
+    p.params.dropout.disableMaskOut = false;
+    p.params.is_inference = true;
+    p.params.softmax_mode = SDPA_DEFAULT_SOFTMAX;
+    p.params.flags = flags;
+    p.fp8_sdpa = dtype == phi::DataType::FLOAT8_E4M3FN;
 
     FusedSdpaProj op(op_info.datatype_);
-    op.AddNode(ct, params);
+    op.AddNode(ct, p);
     op.Compile();
     op_info.setOp(op);
 
@@ -222,14 +302,21 @@ void FusedSdpaProjKernel(const Context& dev_ctx,
 }  // namespace custom_kernel
 
 template <typename Context>
-void CallFusedSdpaProjKernel(const Context& dev_ctx,
-                             const phi::DenseTensor& query_states,
-                             const phi::DenseTensor& key_states,
-                             const phi::DenseTensor& value_states,
-                             const phi::DenseTensor& attn_mask,
-                             const phi::DenseTensor& linear_weights,
-                             phi::DenseTensor* out_linear,
-                             const phi::Scalar& scaling_factor) {
+void CallFusedSdpaProjKernel(
+    const Context& dev_ctx,
+    const phi::DenseTensor& query_states,
+    const phi::DenseTensor& key_states,
+    const phi::DenseTensor& value_states,
+    const phi::DenseTensor& attn_mask,
+    const phi::DenseTensor& linear_weights,
+    phi::DenseTensor* out_linear,
+    const phi::Scalar& scaling_factor,
+    const paddle::optional<phi::DenseTensor>& d_scale_q,
+    const paddle::optional<phi::DenseTensor>& d_scale_k,
+    const paddle::optional<phi::DenseTensor>& d_scale_v,
+    const paddle::optional<phi::DenseTensor>& q_scale_s,
+    const paddle::optional<phi::DenseTensor>& q_scale_o,
+    const paddle::optional<phi::DenseTensor>& d_scale_s) {
   if (query_states.dtype() == phi::DataType::FLOAT16) {
     custom_kernel::FusedSdpaProjKernel<phi::dtype::float16>(dev_ctx,
                                                             query_states,
@@ -238,8 +325,15 @@ void CallFusedSdpaProjKernel(const Context& dev_ctx,
                                                             attn_mask,
                                                             linear_weights,
                                                             out_linear,
-                                                            scaling_factor);
-  } else if (query_states.dtype() == phi::DataType::BFLOAT16) {
+                                                            scaling_factor,
+                                                            d_scale_q,
+                                                            d_scale_k,
+                                                            d_scale_v,
+                                                            q_scale_s,
+                                                            q_scale_o,
+                                                            d_scale_s);
+  } else if (query_states.dtype() == phi::DataType::BFLOAT16 ||
+             query_states.dtype() == phi::DataType::FLOAT8_E4M3FN) {
     custom_kernel::FusedSdpaProjKernel<phi::dtype::bfloat16>(dev_ctx,
                                                              query_states,
                                                              key_states,
@@ -247,18 +341,31 @@ void CallFusedSdpaProjKernel(const Context& dev_ctx,
                                                              attn_mask,
                                                              linear_weights,
                                                              out_linear,
-                                                             scaling_factor);
+                                                             scaling_factor,
+                                                             d_scale_q,
+                                                             d_scale_k,
+                                                             d_scale_v,
+                                                             q_scale_s,
+                                                             q_scale_o,
+                                                             d_scale_s);
   } else {
     throw std::runtime_error("Unsupported data type for FusedSdpaProjKernel");
   }
 }
 
-std::vector<paddle::Tensor> FusedSdpaProj(const paddle::Tensor& query_states,
-                                          const paddle::Tensor& key_states,
-                                          const paddle::Tensor& value_states,
-                                          const paddle::Tensor& attn_mask,
-                                          const paddle::Tensor& linear_weights,
-                                          float scaling_factor) {
+std::vector<paddle::Tensor> FusedSdpaProj(
+    const paddle::Tensor& query_states,
+    const paddle::Tensor& key_states,
+    const paddle::Tensor& value_states,
+    const paddle::Tensor& attn_mask,
+    const paddle::Tensor& linear_weights,
+    float scaling_factor,
+    const paddle::optional<paddle::Tensor>& d_scale_q,
+    const paddle::optional<paddle::Tensor>& d_scale_k,
+    const paddle::optional<paddle::Tensor>& d_scale_v,
+    const paddle::optional<paddle::Tensor>& q_scale_s,
+    const paddle::optional<paddle::Tensor>& q_scale_o,
+    const paddle::optional<paddle::Tensor>& d_scale_s) {
   auto dev_ctx = static_cast<const phi::CustomContext*>(
       paddle::experimental::DeviceContextPool::Instance().Get(
           query_states.place()));
@@ -273,6 +380,54 @@ std::vector<paddle::Tensor> FusedSdpaProj(const paddle::Tensor& query_states,
   auto linear_weights_tensor =
       static_cast<const phi::DenseTensor*>(linear_weights.impl().get());
 
+  // s_scale_q
+  phi::DenseTensor* d_scale_q_tensor = nullptr;
+  if (d_scale_q) {
+    auto d_scale_q_ptr = *(d_scale_q.get_ptr());
+    d_scale_q_tensor =
+        static_cast<phi::DenseTensor*>(d_scale_q_ptr.impl().get());
+  }
+
+  // d_scale_k
+  phi::DenseTensor* d_scale_k_tensor = nullptr;
+  if (d_scale_k) {
+    auto d_scale_k_ptr = *(d_scale_k.get_ptr());
+    d_scale_k_tensor =
+        static_cast<phi::DenseTensor*>(d_scale_k_ptr.impl().get());
+  }
+
+  // d_scale_v
+  phi::DenseTensor* d_scale_v_tensor = nullptr;
+  if (d_scale_v) {
+    auto d_scale_v_ptr = *(d_scale_v.get_ptr());
+    d_scale_v_tensor =
+        static_cast<phi::DenseTensor*>(d_scale_v_ptr.impl().get());
+  }
+
+  // q_scale_s
+  phi::DenseTensor* q_scale_s_tensor = nullptr;
+  if (q_scale_s) {
+    auto q_scale_s_ptr = *(q_scale_s.get_ptr());
+    q_scale_s_tensor =
+        static_cast<phi::DenseTensor*>(q_scale_s_ptr.impl().get());
+  }
+
+  // q_scale_o
+  phi::DenseTensor* q_scale_o_tensor = nullptr;
+  if (q_scale_o) {
+    auto q_scale_o_ptr = *(q_scale_o.get_ptr());
+    q_scale_o_tensor =
+        static_cast<phi::DenseTensor*>(q_scale_o_ptr.impl().get());
+  }
+
+  // d_scale_s
+  phi::DenseTensor* d_scale_s_tensor = nullptr;
+  if (d_scale_s) {
+    auto d_scale_s_ptr = *(d_scale_s.get_ptr());
+    d_scale_s_tensor =
+        static_cast<phi::DenseTensor*>(d_scale_s_ptr.impl().get());
+  }
+
   // allocate memory on device.
   int64_t bsz = query_states.dims()[0];
   int64_t seq_len = query_states.dims()[2];
@@ -281,16 +436,28 @@ std::vector<paddle::Tensor> FusedSdpaProj(const paddle::Tensor& query_states,
   std::shared_ptr<phi::DenseTensor> out_linear =
       std::make_shared<phi::DenseTensor>();
   out_linear->Resize(phi::make_ddim({bsz, seq_len, hidden_size}));
-  dev_ctx->Alloc(out_linear.get(), query_states_tensor->dtype());
 
-  CallFusedSdpaProjKernel(*dev_ctx,
-                          *query_states_tensor,
-                          *key_states_tensor,
-                          *value_states_tensor,
-                          *attn_mask_tensor,
-                          *linear_weights_tensor,
-                          out_linear.get(),
-                          phi::Scalar(scaling_factor));
+  if (query_states_tensor->dtype() == phi::DataType::FLOAT8_E4M3FN) {
+    dev_ctx->Alloc(out_linear.get(), phi::DataType::BFLOAT16);
+  } else {
+    dev_ctx->Alloc(out_linear.get(), query_states_tensor->dtype());
+  }
+
+  CallFusedSdpaProjKernel(
+      *dev_ctx,
+      *query_states_tensor,
+      *key_states_tensor,
+      *value_states_tensor,
+      *attn_mask_tensor,
+      *linear_weights_tensor,
+      out_linear.get(),
+      phi::Scalar(scaling_factor),
+      d_scale_q ? *d_scale_q_tensor : paddle::optional<phi::DenseTensor>(),
+      d_scale_k ? *d_scale_k_tensor : paddle::optional<phi::DenseTensor>(),
+      d_scale_v ? *d_scale_v_tensor : paddle::optional<phi::DenseTensor>(),
+      q_scale_s ? *q_scale_s_tensor : paddle::optional<phi::DenseTensor>(),
+      q_scale_o ? *q_scale_o_tensor : paddle::optional<phi::DenseTensor>(),
+      d_scale_s ? *d_scale_s_tensor : paddle::optional<phi::DenseTensor>());
   return {paddle::Tensor(out_linear)};
 }
 
@@ -320,7 +487,13 @@ PD_BUILD_OP(fused_sdpa_proj)
              "key_states",
              "value_states",
              "attn_mask",
-             "linear_weights"})
+             "linear_weights",
+             paddle::Optional("d_scale_q"),
+             paddle::Optional("d_scale_k"),
+             paddle::Optional("d_scale_v"),
+             paddle::Optional("q_scale_s"),
+             paddle::Optional("q_scale_o"),
+             paddle::Optional("d_scale_s")})
     .Outputs({"out_linear"})
     .Attrs({"scaling_factor: float"})
     .SetKernelFn(PD_KERNEL(FusedSdpaProj))
--- backends/intel_hpu/tests/unittests/test_fused_sdpa_proj.py
+++ backends/intel_hpu/tests/unittests/test_fused_sdpa_proj.py
@@ -19,6 +19,8 @@ import paddle
 import paddlenlp_ops
 
 import os
+import math
+import paddle.nn.functional as F
 
 intel_hpus_module_id = os.environ.get("FLAGS_selected_intel_hpus", 0)
 
@@ -49,7 +51,7 @@ def fused_sdpa_proj(
     return out_linear_out
 
 
-class TestSdpa_Proj_OpFP32(unittest.TestCase):
+class TestSdpa_Proj(unittest.TestCase):
     def setUp(self):
         self.init_dtype()
         self.batch_size = 5
@@ -94,7 +96,8 @@ class TestSdpa_Proj_OpFP32(unittest.TestCase):
         ).to(paddle.bfloat16)
 
         attn_mask = paddle.ones(
-            [1, 1, max_seq_length, max_seq_length], dtype=paddle.bfloat16
+            [batch_size, num_heads, max_seq_length, max_seq_length],
+            dtype=paddle.bfloat16,
         )
         attn_mask = paddle.tril(attn_mask)
         attn_mask = (1.0 - attn_mask) * -10000.0
@@ -118,11 +121,17 @@ class TestSdpa_Proj_OpFP32(unittest.TestCase):
             attention_mask,
             linear_weights,
             scaling_factor=self.head_dim**-0.5,
+            d_scale_q=None,
+            d_scale_k=None,
+            d_scale_v=None,
+            q_scale_s=None,
+            q_scale_o=None,
+            d_scale_s=None,
         )
         return out_fused_sdpa_proj_tensor
 
-    def check_result(self, torch_result, ops_result):
-        np.testing.assert_allclose(torch_result, ops_result)
+    def check_result(self, torch_result, ops_result, rtol=1e-07):
+        np.testing.assert_allclose(torch_result, ops_result, rtol)
 
     def test_fused_sdpa_proj(self):
         batch_size = 5
@@ -157,6 +166,124 @@ class TestSdpa_Proj_OpFP32(unittest.TestCase):
         )
 
         self.check_result(torch_result.numpy(), custom_op_result)
+
+
+class Sdpa_Proj_Fp8(TestSdpa_Proj):
+    def get_scale_values(self, t, is_t_amax=False):
+        FP8_MAX_143 = 240 * 0.9
+        if is_t_amax is False:
+            maxT = paddle.max(paddle.abs(t)).to(paddle.float32).item()
+        else:
+            maxT = t.item()
+        scaleT = FP8_MAX_143 / maxT
+
+        lg2 = math.log2(scaleT)
+        lg2_int = int(lg2)
+
+        scaleT_pow2 = 2.0**lg2_int
+        scaleTInv = 1.0 / scaleT_pow2
+
+        return scaleT_pow2, scaleTInv
+
+    def get_max_weight(
+        self,
+        query,
+        key,
+        scale=None,
+    ):
+        sqrt_dim_head = query.shape[-1] ** 0.5
+        scores = paddle.matmul(
+            query,
+            key,
+            transpose_x=False,
+            transpose_y=True,
+        )
+        if scale is None:
+            scores = scores / sqrt_dim_head
+        else:
+            scores = scores * scale
+
+        weight = F.softmax(scores, axis=-1)
+
+        return paddle.max(paddle.abs(weight)).to(paddle.float32)
+
+    def fused_sdpa_proj_op_custom(
+        self, query_states, key_states, value_states, attn_mask, linear_weights
+    ):
+        attention_mask = attn_mask[..., : self.seq_length, : self.kv_seq_len]
+        attention_mask = attention_mask.astype(paddle.bfloat16)
+
+        scaleQ, scaleQInv = self.get_scale_values(query_states)
+        scaleK, scaleKInv = self.get_scale_values(key_states)
+        scaleV, scaleVInv = self.get_scale_values(value_states)
+
+        with paddle.amp.auto_cast(dtype="bfloat16", enable=True):
+            amax_s_ref = self.get_max_weight(query_states, key_states, scale=None)
+
+        scaleS, scaleSInv = self.get_scale_values(amax_s_ref, is_t_amax=True)
+
+        q_fp8 = (scaleQ * query_states).astype(paddle.float8_e4m3fn)
+        k_fp8 = (scaleK * key_states).astype(paddle.float8_e4m3fn)
+        v_fp8 = (scaleV * value_states).astype(paddle.float8_e4m3fn)
+        linear_weights_fp8 = linear_weights.astype(paddle.float8_e4m3fn)
+
+        d_scale_q = paddle.to_tensor([scaleQInv])
+        d_scale_k = paddle.to_tensor([scaleKInv])
+        d_scale_v = paddle.to_tensor([scaleVInv])
+        q_scale_s = paddle.to_tensor([scaleS])
+        q_scale_o = None
+        d_scale_s = paddle.to_tensor([scaleSInv])
+
+        out_fused_sdpa_proj_tensor = paddlenlp_ops.fused_sdpa_proj(
+            q_fp8,
+            k_fp8,
+            v_fp8,
+            attention_mask,
+            linear_weights_fp8,
+            scaling_factor=self.head_dim**-0.5,
+            d_scale_q=d_scale_q,
+            d_scale_k=d_scale_k,
+            d_scale_v=d_scale_v,
+            q_scale_s=q_scale_s,
+            q_scale_o=q_scale_o,
+            d_scale_s=d_scale_s,
+        )
+
+        return out_fused_sdpa_proj_tensor
+
+    def test_fused_sdpa_proj(self):
+        batch_size = 5
+        num_heads = 32
+        seq_length = 1
+        head_dim = 128
+        kv_seq_len = 5
+        max_seq_length = 100
+        (
+            query_states,
+            key_states,
+            value_states,
+            attn_mask,
+            linear_weights,
+        ) = self.prepare_input(
+            batch_size, num_heads, seq_length, head_dim, kv_seq_len, max_seq_length
+        )
+
+        custom_op_result = self.fused_sdpa_proj_op_custom(
+            query_states, key_states, value_states, attn_mask, linear_weights
+        )
+
+        attention_mask = attn_mask[..., : self.seq_length, : self.kv_seq_len]
+        attention_mask = attention_mask.astype(query_states.dtype)
+        torch_result = fused_sdpa_proj(
+            query_states.transpose([0, 2, 1, 3]),
+            key_states.transpose([0, 2, 1, 3]),
+            value_states.transpose([0, 2, 1, 3]),
+            attention_mask,
+            linear_weights,
+            scaling_factor=head_dim**-0.5,
+        )
+
+        self.check_result(torch_result.numpy(), custom_op_result, rtol=1e-2)
 
 
 if __name__ == "__main__":

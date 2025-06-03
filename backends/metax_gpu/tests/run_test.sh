b88789df3e66373f3e4a7342b744876e00213b8a
sw <1640472053@qq.com>
2025-06-03 16:41:52 +0800

    rm_metax

--- backends/metax_gpu/tests/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export PATH=/usr/local/corex-4.3.0/bin:$PATH
-# export LD_LIBRARY_PATH=/usr/local/corex-4.3.0/lib
-# export LIBRARY_PATH=/usr/local/corex-4.3.0/lib
-export PYTHONPATH=${PYTHONPATH}:${PADDLE_SOURCE_DIR}/test/legacy_test
-
-mkdir -p build && cd build && cmake ..
-make run_tests

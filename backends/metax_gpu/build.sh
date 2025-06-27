038a26257899ee1f8dea67c54a9e6db9d65f7aad
qimeng du <1640472053@qq.com>
2025-06-27 09:39:25 +0000

    chang_build.sh

--- backends/metax_gpu/build.sh
+++ backends/metax_gpu/build.sh
@@ -24,7 +24,7 @@ export http_proxy=http://10.2.192.21:1080 https_proxy=http://10.2.192.21:1080
 # install paddle
 python -m pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
 
-exit 1
+# exit 1
 # init paddle
 git submodule sync --recursive && git submodule update --init --recursive
 

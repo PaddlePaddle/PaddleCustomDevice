59aa15befff463a5529446fb8eceaa6795354348
qimeng du <1640472053@qq.com>
2025-07-09 12:23:32 +0000

    add_kernel_fix_paddlebug

--- backends/metax_gpu/kernels/cuda_kernels/conv_grad_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/conv_grad_kernel_register.cu
@@ -48,15 +48,15 @@ void Conv3DGradKernel(const Context& dev_ctx,
 
 }  // namespace phi
 
-PD_REGISTER_KERNEL(
-    conv2d_grad, GPU, ALL_LAYOUT, phi::ConvGradKernel, float, double) {}
+PD_REGISTER_PLUGIN_KERNEL(
+    conv2d_grad, metax_gpu, ALL_LAYOUT, phi::ConvGradKernel, float, double) {}
 
-PD_REGISTER_KERNEL(
-    conv3d_grad, GPU, ALL_LAYOUT, phi::Conv3DGradKernel, float, double) {}
+PD_REGISTER_PLUGIN_KERNEL(
+    conv3d_grad, metax_gpu, ALL_LAYOUT, phi::Conv3DGradKernel, float, double) {}
 
-PD_REGISTER_KERNEL(conv2d_double_grad,
-                   GPU,
-                   ALL_LAYOUT,
-                   phi::ConvGradGradKernel,
-                   float,
-                   double) {}
+PD_REGISTER_PLUGIN_KERNEL(conv2d_double_grad,
+                          metax_gpu,
+                          ALL_LAYOUT,
+                          phi::ConvGradGradKernel,
+                          float,
+                          double) {}

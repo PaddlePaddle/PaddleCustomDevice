7e8097d102e56c9b20ad98526120982c9746cd10
xxiu1 <1923295312@qq.com>
2025-10-23 12:46:53 +0000

    fix psroi_pool_grad_kernel registration

--- backends/iluvatar_gpu/kernels/cuda_kernels/psroi_pool_grad_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/psroi_pool_grad_kernel_register.cu
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/gpu/psroi_pool_grad_kernel.cu"  //NOLINT
+#include "paddle/phi/kernels/psroi_pool_grad_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(psroi_pool_grad,
                           iluvatar_gpu,

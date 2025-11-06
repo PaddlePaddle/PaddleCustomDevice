03ed82483877acbebcae683ec3298397728adc11
geyuqiang <geyuqiang@baidu.com>
2025-11-06 19:43:08 +0800

    restore protobuf

--- backends/iluvatar_gpu/cmake/external/protobuf.cmake
+++ backends/iluvatar_gpu/cmake/external/protobuf.cmake
@@ -293,8 +293,7 @@ function(build_protobuf TARGET_NAME)
       SOURCE_DIR ${SOURCE_DIR}
       UPDATE_COMMAND ""
       PATCH_COMMAND
-      COMMAND cd ${SOURCE_DIR} && git fetch --all --tags && git checkout
-              ${PROTOBUF_TAG}
+      COMMAND cd ${SOURCE_DIR} && git checkout ${PROTOBUF_TAG}
       DEPENDS zlib
       CONFIGURE_COMMAND
         ${CMAKE_COMMAND} ${SOURCE_DIR}/cmake ${OPTIONAL_ARGS}

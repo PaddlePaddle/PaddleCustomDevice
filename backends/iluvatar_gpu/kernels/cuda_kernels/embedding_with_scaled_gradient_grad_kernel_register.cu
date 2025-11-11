e4a7455416310c74dc8ce9e9f42f4ad2c4d42c04
LiaoYFBH <2273398935@qq.com>
2025-11-11 12:18:08 +0800

    fix: Correct .cu include and CMake for embedding_with_scaled_gradient_grad kernel.

--- backends/iluvatar_gpu/kernels/cuda_kernels/embedding_with_scaled_gradient_grad_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/embedding_with_scaled_gradient_grad_kernel_register.cu
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/gpu/embedding_with_scaled_gradient_grad_kernel.cu"  // NOLINT
+#include "paddle/phi/kernels/gpu/embedding_with_scaled_gradient_grad_kernel.h"  // NOLINT
 
 PD_CUSTOM_KERNEL_REGISTER(embedding_with_scaled_gradient_grad,
                           iluvatar_gpu,

e9e080b99ab0a44e50c49b1533d6dbc7b1163126
geyuqiang <geyuqiang@baidu.com>
2025-11-12 15:56:38 +0800

    fix iluvatar patch

--- backends/metax_gpu/kernels/cuda_kernels/collect_fpn_proposals_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/collect_fpn_proposals_kernel_register.cu
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/gpu/collect_fpn_proposals_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(collect_fpn_proposals,

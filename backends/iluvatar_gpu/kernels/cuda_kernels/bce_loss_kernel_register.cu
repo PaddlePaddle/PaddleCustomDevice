823e99fec52dc11477a05c8617997953d2a41092
Jingzong Liu <470699397@qq.com>
2025-12-05 00:16:40 +0800

    Add include for kernel registry in bce_loss_kernel

--- backends/iluvatar_gpu/kernels/cuda_kernels/bce_loss_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/bce_loss_kernel_register.cu
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#include "paddle/phi/core/kernel_registry.h"
 #include "paddle/phi/kernels/bce_loss_kernel.h"  // NOLINT
 
 PD_CUSTOM_KERNEL_REGISTER(bce_loss,

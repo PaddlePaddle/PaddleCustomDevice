ddb740bd054409df6a1b040ea5932e779b40f70d
LiaoYFBH <2273398935@qq.com>
2025-12-05 14:35:50 +0800

    Removes  from .

--- backends/iluvatar_gpu/kernels/cuda_kernels/quantize_linear_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/quantize_linear_kernel_register.cu
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/impl/quantize_linear_impl.h"
 #include "paddle/phi/kernels/quantize_linear_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(dequantize_linear,

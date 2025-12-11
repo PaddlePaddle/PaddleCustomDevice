6627eb942332a0e44f7b9db7306c0c10b9a1e16d
SidusAntares <2934978005@qq.com>
2025-12-11 08:21:19 +0800

    【CUDA Kernel No.79】paddle/phi/kernels/gpu/matrix_power_kernel.cu算子Kernel修复

--- backends/metax_gpu/kernels/cuda_kernels/matrix_power_kernel_register.cu
+++ backends/metax_gpu/kernels/cuda_kernels/matrix_power_kernel_register.cu
@@ -13,7 +13,10 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 #include "paddle/phi/core/kernel_registry.h"
-#include "paddle/phi/kernels/gpu/matrix_power_kernel.cu"  // NOLINT
+#include "paddle/phi/kernels/matrix_power_kernel.h"
+
+#include "paddle/phi/backends/gpu/gpu_context.h"
+#include "paddle/phi/kernels/impl/matrix_power_kernel_impl.h"
 
 PD_CUSTOM_KERNEL_REGISTER(matrix_power,
                           metax_gpu,

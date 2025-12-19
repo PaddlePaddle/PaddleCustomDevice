3f6d39985e34da90fc4926433f36e01b2a742eb6
geyuqiang <geyuqiang@baidu.com>
2025-12-19 04:17:17 +0000

    Update paddle

--- backends/mlu/tools/pr_ci_mlu.sh
+++ backends/mlu/tools/pr_ci_mlu.sh
@@ -264,7 +264,7 @@ function main() {
     unset MLU_VISIBLE_DEVICES
     echo "Start Download"
     git clone --depth 1000 https://gitee.com/PaddlePaddle/PaddleX.git
-    pip install pymupdf pypdfium2
+    pip install pymupdf pypdfium2 ruamel.yaml
     cd PaddleX
     pip install -e .
     paddlex --install PaddleClas

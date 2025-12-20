010ba26866565cbc23f762df8e4d0b6f32c73c6a
Frank Lee <lipeijiegg@gmail.com>
2025-12-21 01:11:46 +0800

    【CUDA Kernel No.111】paddle/phi/kernels/gpu/stack_grad_kernel.cu算子Kernel修复

--- backends/iluvatar_gpu/kernels/cuda_kernels/stack_grad_kernel_register.cu
+++ backends/iluvatar_gpu/kernels/cuda_kernels/stack_grad_kernel_register.cu
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "paddle/phi/kernels/gpu/stack_grad_kernel.cu"  // NOLINT
+#include "paddle/phi/kernels/stack_grad_kernel.h"
 
 PD_CUSTOM_KERNEL_REGISTER(stack_grad,
                           iluvatar_gpu,

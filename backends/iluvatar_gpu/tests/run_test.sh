e8c2eb5ecf062a2d0655382662b6390679846b6c
geyuqiang <geyuqiang@baidu.com>
2026-02-10 06:51:57 +0000

    remove nvjepg test

--- backends/iluvatar_gpu/tests/run_test.sh
+++ backends/iluvatar_gpu/tests/run_test.sh
@@ -23,9 +23,9 @@ echo ""
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 LEGACY_TEST_PATH="${SCRIPT_DIR}/../../../Paddle/test/legacy_test"
 
-# export PATH=/usr/local/corex/bin:$PATH
-# export LD_LIBRARY_PATH=/usr/local/corex/lib
-# export LIBRARY_PATH=/usr/local/corex/lib
+export PATH=/usr/local/corex/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/corex/lib
+export LIBRARY_PATH=/usr/local/corex/lib
 export PYTHONPATH="${LEGACY_TEST_PATH}:${PYTHONPATH}"
 
 python -m pip install parameterized

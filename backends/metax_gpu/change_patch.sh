574b10f40a1e932ed17ea266318d42bb3b2e07d9
co63oc <4617245+co63oc@users.noreply.github.com>
2026-04-17 14:42:15 +0800

    remove tmp/mixed_vector.h

--- backends/metax_gpu/change_patch.sh
+++ backends/metax_gpu/change_patch.sh
@@ -22,7 +22,6 @@ mv Eigen_3.4.0_paddle eigen3
 cd ..
 cp -r patch/eigen3/ ../../Paddle/third_party/eigen3
 rm -r patch/eigen3
-# cp patch/tmp/mixed_vector* ../../Paddle/paddle/phi/core
 cd ../../Paddle/
 git apply --verbose ../backends/metax_gpu/patch/paddle.patch
 cd -
